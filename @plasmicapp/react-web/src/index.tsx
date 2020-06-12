5bb33658e3297330070709a98d97a5616c0657a3
Chung Wu <chungwu@plasmic.app>
2020-06-12 13:01:25 -0700

    [slots] Use plain text slot args, add string wrapper in PlasmicSlot
    Change-Id: Ie7f726d56a1a88caab8f73e2f9f42681232646db

--- @plasmicapp/react-web/src/index.tsx
+++ @plasmicapp/react-web/src/index.tsx
@@ -259,6 +259,59 @@ export function PlasmicIcon(
   return <PlasmicIconType {...rest} />;
 }
 
+function maybeAsString(node: React.ReactNode): string | undefined {
+  // Unwrap fragments
+  if (React.isValidElement(node) && node.type === React.Fragment) {
+    return maybeAsString(node.props.children);
+  }
+
+  if (typeof node === "string") {
+    return node;
+  }
+
+  if (Array.isArray(node) && node.length === 1 && typeof node[0] === "string") {
+    return node[0];
+  }
+
+  return undefined;
+}
+
+export function PlasmicSlot<T extends keyof JSX.IntrinsicElements = "div">(
+  props: React.ComponentProps<T> & {
+    as?: T;
+    defaultContents?: React.ReactNode;
+    value?: React.ReactNode;
+  }
+) {
+  const { as, defaultContents, value, ...rest } = props;
+
+  let content = value || defaultContents;
+  if (!content || (Array.isArray(content) && content.length === 0)) {
+    return null;
+  }
+
+  // If the content is a raw string, then we need to wrap the raw string
+  // into an element, in case the slot is inside a flex-gap
+  // container (you cannot apply margin to just a text node).
+  const maybeString = maybeAsString(content);
+  if (maybeString) {
+    content = <div className="__wab_slot-string-wrapper">{maybeString}</div>;
+  }
+
+  const nonEmptyProps = Object.keys(rest).filter(p => !!(rest as any)[p]);
+  if (nonEmptyProps.length === 0) {
+    // No attrs to apply to the slot (which means the slot is unstyled), then
+    // just render the content directly; no need for style wrapper.
+    return <>{content}</>;
+  }
+
+  return React.createElement(
+    as || "div",
+    mergeProps({ className: "__wab_slot" }, rest),
+    content
+  );
+}
+
 // To get a type safe FlexStack, you need to explicitly specify both the tag
 // and generic type, such as
 //   <StackImpl as={"a"} href={...}>...</FlexStack>
