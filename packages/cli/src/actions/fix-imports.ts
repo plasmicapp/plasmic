bf081136d583d759526397ff9581a6155824c1fa
Tiago Madeira <tiago@plasmic.app>
2021-04-09 10:25:08 -0300

    [cli] Skip auth on fix-imports
    `plasmic fix-imports` is currently requiring auth; this patch fixes
    that.
    
    Clubhouse issue:
    https://app.clubhouse.io/plasmic/story/11654/
    
    Change-Id: I9556281e4c6605f111e2c09010f136dbac41103d

--- packages/cli/src/actions/fix-imports.ts
+++ packages/cli/src/actions/fix-imports.ts
@@ -5,7 +5,7 @@ import { getContext } from "../utils/get-context";
 
 export interface FixImportsArgs extends CommonArgs {}
 export async function fixImports(opts: FixImportsArgs) {
-  const context = await getContext(opts);
+  const context = await getContext(opts, { enableSkipAuth: true });
 
   await fixAllFilePaths(context);
   await fixAllImportStatements(context);
