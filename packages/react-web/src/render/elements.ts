256d92bdf31ae56bda3f15f26566244c336f7bcb
Victor Agnez Lima <victor@plasmic.app>
2021-05-19 16:34:25 -0300

    [react-web] Allow array children
    Change-Id: I29e0e1fff778f33f693bad53bbf7dcd1a247d79c

--- packages/react-web/src/render/elements.ts
+++ packages/react-web/src/render/elements.ts
@@ -251,7 +251,7 @@ function createPlasmicElementFromJsx<
     defaultElement,
     mergeProps(
       props,
-      children.length === 0 ? {} : { children: ensureNotArray(children) },
+      children.length === 0 ? {} : { children: children.length === 1 ? children[0] : children },
       ...triggerProps
     ) as any,
     wrapFlexChild
