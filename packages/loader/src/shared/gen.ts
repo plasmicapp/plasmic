7582ce0fb41ffb9c03be5cb9b074c0615ff7d285
clovis <clovis@plasmic.app>
2021-05-19 15:43:59 -0500

    Improve the way we manage the CLI config data
    Small refactoring in preparation for improved types
    
    Change-Id: I24f197433e23d47440d3a68bbc521712ea7514c8

--- packages/loader/src/shared/gen.ts
+++ packages/loader/src/shared/gen.ts
@@ -1,7 +1,6 @@
 import { promises as fs } from "fs";
 import path from "upath";
 import templates from "../templates";
-import * as logger from "./logger";
 
 type ComponentData = {
   name: string;
@@ -10,7 +9,11 @@ type ComponentData = {
   type: string;
 };
 
-type PageData = ComponentData & { url: string; type: "page" };
+type PageData = ComponentData & {
+  url: string;
+  skeletonPath: string;
+  type: "page";
+};
 
 type GenOptions = {
   dir: string;
@@ -19,44 +22,113 @@ type GenOptions = {
 
 type ProviderData = ComponentData & { providerName?: string };
 
+type ConfigData = {
+  componentData: Array<ComponentData | PageData>;
+  componentDataKeyedByName: Record<string, (ComponentData | PageData)[]>;
+  providerData: ProviderData[];
+  providersKeyedByName: Record<string, ProviderData[]>;
+};
+
+function isPageData(data: ComponentData | PageData): data is PageData {
+  return data.type === "page";
+}
+
 function writeFile(filePath: string, content: string) {
   return fs
     .mkdir(path.dirname(filePath), { recursive: true })
     .then(() => fs.writeFile(filePath, content));
 }
 
-export async function generateAll(opts: GenOptions) {
+export async function getConfigData(opts: GenOptions): Promise<ConfigData> {
   const configPath = path.join(opts.dir, "plasmic.json");
   const configData = await fs.readFile(configPath);
   const config = JSON.parse(configData.toString());
 
+  const componentData: Array<ComponentData | PageData> = [];
+  const componentDataKeyedByName: {
+    [name: string]: (ComponentData | PageData)[];
+  } = {};
+
+  const addToComponentData = (data: ComponentData | PageData) => {
+    if (!componentDataKeyedByName[data.name]) {
+      componentDataKeyedByName[data.name] = [];
+    }
+    componentData.push(data);
+    componentDataKeyedByName[data.name].push(data);
+  };
+
+  for (const project of config.projects) {
+    project.components.forEach((component: any) => {
+      addToComponentData({
+        name: component.name,
+        projectId: project.projectId,
+        path: path.join(
+          opts.dir,
+          config.srcDir,
+          component.renderModuleFilePath
+        ),
+        type: component.componentType,
+        skeletonPath: path.join(
+          opts.dir,
+          config.srcDir,
+          component.importSpec.modulePath
+        ),
+        url:
+          component.componentType === "page"
+            ? getPageUrl(component.importSpec.modulePath)
+            : undefined,
+      });
+    });
+    project.icons.forEach((icon: any) =>
+      addToComponentData({
+        name: icon.name,
+        projectId: project.projectId,
+        path: path.join(opts.dir, config.srcDir, icon.moduleFilePath),
+        type: icon.componentType,
+      })
+    );
+  }
+
+  const providerData: ProviderData[] = [];
+  const providersKeyedByName: { [name: string]: ProviderData[] } = {};
+
+  for (const provider of config.globalVariants?.variantGroups || []) {
+    const data = {
+      name: provider.name,
+      projectId: provider.projectId,
+      path: path.join(opts.dir, config.srcDir, provider.contextFilePath),
+      providerName: provider.name === "Screen" ? "ScreenVariantProvider" : "",
+      type: "provider",
+    };
+    if (!providersKeyedByName[data.name]) {
+      providersKeyedByName[data.name] = [];
+    }
+    providerData.push(data);
+    providersKeyedByName[data.name].push(data);
+  }
+
+  return {
+    componentData,
+    componentDataKeyedByName,
+    providerData,
+    providersKeyedByName,
+  };
+}
+
+export async function generateAll(opts: GenOptions) {
+  const configData = await getConfigData(opts);
+
   await Promise.all([
-    generatePlasmicLoader(opts.dir, config),
-    generatePageComponents(opts.pageDir, config),
+    generatePlasmicLoader(configData),
+    generatePageComponents(configData),
   ]);
 }
 
-async function generatePageComponents(dir: string, config: any) {
-  const componentData: ComponentData[] = [];
-  for (const project of config.projects) {
-    for (const component of project.components) {
-      if (component.componentType !== "page") {
-        continue;
-      }
-      const [_, componentPath] = component.importSpec.modulePath.split(/pages/);
-      componentData.push({
-        name: component.name,
-        projectId: project.projectId,
-        path: path.join(dir, componentPath),
-        type: component.componentType,
-      });
-    }
-  }
-
-  await Promise.all(
-    componentData.map((data) =>
+function generatePageComponents(config: ConfigData) {
+  return Promise.all(
+    config.componentData.filter(isPageData).map((data) =>
       writeFile(
-        data.path,
+        data.skeletonPath,
         templates.PlasmicPage({
           name: data.name,
           projectId: data.projectId,
@@ -82,78 +154,27 @@ function getPageUrl(path: string) {
   return url === "" ? "/" : url;
 }
 
-function generatePlasmicLoader(dir: string, config: any) {
+function generatePlasmicLoader(config: ConfigData) {
   const entrypointPath = path.join(__dirname, "../", "PlasmicLoader.jsx");
-  const componentData: Array<ComponentData | PageData> = [];
-  const componentDataKeyedByName: { [name: string]: ComponentData[] } = {};
-
-  const addToComponentData = (data: ComponentData | PageData) => {
-    if (!componentDataKeyedByName[data.name]) {
-      componentDataKeyedByName[data.name] = [];
-    }
-    componentData.push(data);
-    componentDataKeyedByName[data.name].push(data);
-  };
-
-  for (const project of config.projects) {
-    project.components.forEach((component: any) => {
-      addToComponentData({
-        name: component.name,
-        projectId: project.projectId,
-        path: path.join(dir, config.srcDir, component.renderModuleFilePath),
-        type: component.componentType,
-        url:
-          component.componentType === "page"
-            ? getPageUrl(component.importSpec.modulePath)
-            : undefined,
-      });
-    });
-    project.icons.forEach((icon: any) =>
-      addToComponentData({
-        name: icon.name,
-        projectId: project.projectId,
-        path: path.join(dir, config.srcDir, icon.moduleFilePath),
-        type: icon.componentType,
-      })
-    );
-  }
-
-  const providerData: ProviderData[] = [];
-  const providersKeyedByName: { [name: string]: ProviderData[] } = {};
-
-  for (const provider of config.globalVariants?.variantGroups || []) {
-    const data = {
-      name: provider.name,
-      projectId: provider.projectId,
-      path: path.join(dir, config.srcDir, provider.contextFilePath),
-      providerName: provider.name === "Screen" ? "ScreenVariantProvider" : "",
-      type: "provider",
-    };
-    if (!providersKeyedByName[data.name]) {
-      providersKeyedByName[data.name] = [];
-    }
-    providerData.push(data);
-    providersKeyedByName[data.name].push(data);
-  }
 
   return writeFile(
     entrypointPath,
     templates.PlasmicLoader({
-      componentData,
-      pagesByUrl: componentData.filter((component) => component.type === "page"),
-      componentsWithOneProject: Object.values(componentDataKeyedByName)
+      componentData: config.componentData,
+      pagesByUrl: config.componentData.filter(isPageData),
+      componentsWithOneProject: Object.values(config.componentDataKeyedByName)
         .filter((components) => components.length === 1)
         .flat(),
-      componentMap: Object.entries(
-        componentDataKeyedByName
-      ).map(([name, projects]) => ({ name, projects })),
-      providerData,
-      providersWithOneProject: Object.values(providersKeyedByName)
+      componentMap: Object.entries(config.componentDataKeyedByName).map(
+        ([name, projects]) => ({ name, projects })
+      ),
+      providerData: config.providerData,
+      providersWithOneProject: Object.values(config.providersKeyedByName)
         .filter((providers) => providers.length === 1)
         .flat(),
-      providerMap: Object.entries(
-        providersKeyedByName
-      ).map(([name, projects]) => ({ name, projects })),
+      providerMap: Object.entries(config.providersKeyedByName).map(
+        ([name, projects]) => ({ name, projects })
+      ),
     })
   );
 }
