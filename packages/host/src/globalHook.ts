3cbed4ecf42e32f0661ccb5a48e10be2e43e5633
Victor Agnez Lima <victor@plasmic.app>
2021-06-10 12:11:36 -0300

    [host] Avoid crash when the global hook is not available
    Change-Id: Idb72077fad8fec4f4d77453e6413f722fed6b243

--- packages/host/src/globalHook.ts
+++ packages/host/src/globalHook.ts
@@ -4,130 +4,107 @@ import { traverseTree, traverseUpdates } from "./traverseFiber";
 
 const root = require("window-or-global");
 
-const searchParams = (() => {
-  try {
-    return new URLSearchParams(
-      location.search ||
-        new URL(location.toString().replace(/#/, "?")).searchParams.get(
-          "searchParams"
-        ) ||
-        window?.parent?.location.search ||
-        ""
-    );
-  } catch {
-    return new URLSearchParams();
-  }
-})();
-
 function hasKey(v: any, key: string) {
   return typeof v === "object" && v !== null && key in v; 
 }
 
-// For now we only use our hook when enabling code components as it might
-// impact performance
-const codeComponents = searchParams.get("codeComponents") === "true";
-
 const officialHook = (root as any).__REACT_DEVTOOLS_GLOBAL_HOOK__;
 
-if (codeComponents) {
-  assert(!!officialHook);
+if (officialHook && !officialHook.plasmic) {
+  const officialHookProps = { ...officialHook };
+  const valNodeData = "data-plasmic-valnode";
+  const valNodeDispose = "data-plasmic-dispose";
 
-  if (!officialHook.plasmic) {
-    const officialHookProps = { ...officialHook };
-    const valNodeData = "data-plasmic-valnode";
-    const valNodeDispose = "data-plasmic-dispose";
+  officialHook.plasmic = {
+    uidToFiber: new Map<number, Fiber>(),
+  };
+  const tryGetValNodeUid = (node: Fiber): number | undefined => {
+    if (hasKey(node.memoizedProps, valNodeData)) {
+      return +node.memoizedProps[valNodeData];
+    }
+    return undefined;
+  };
 
-    officialHook.plasmic = {
-      uidToFiber: new Map<number, Fiber>(),
-    };
-    const tryGetValNodeUid = (node: Fiber): number | undefined => {
-      if (hasKey(node.memoizedProps, valNodeData)) {
-        return +node.memoizedProps[valNodeData];
-      }
-      return undefined;
-    };
+  const addNode = (node: Fiber) => {
+    const valNodeUid = tryGetValNodeUid(node);
+    if (valNodeUid) {
+      officialHook.plasmic.uidToFiber.set(valNodeUid, node);
+    }
+  };
 
-    const addNode = (node: Fiber) => {
-      const valNodeUid = tryGetValNodeUid(node);
-      if (valNodeUid) {
-        officialHook.plasmic.uidToFiber.set(valNodeUid, node);
-      }
-    };
+  const rmNode = (node: Fiber) => {
+    if (hasKey(node.memoizedProps, valNodeDispose)) {
+      const dispose: () => void = node.memoizedProps[valNodeDispose];
+      dispose();
+    }
+  };
 
-    const rmNode = (node: Fiber) => {
-      if (hasKey(node.memoizedProps, valNodeDispose)) {
-        const dispose: () => void = node.memoizedProps[valNodeDispose];
-        dispose();
-      }
-    };
+  // Return true if this is a canvas frame's tree.
+  const isCanvasFrame = (containerInfo: any) => {
+    const doc: Document | undefined =
+      containerInfo?.ownerDocument || containerInfo;
+    if (doc && doc.getElementById) {
+      return !!doc.getElementById("plasmic-app");
+    }
+    assert(false, "Unreachable code");
+  };
 
-    // Return true if this is a canvas frame's tree.
-    const isCanvasFrame = (containerInfo: any) => {
-      const doc: Document | undefined =
-        containerInfo?.ownerDocument || containerInfo;
-      if (doc && doc.getElementById) {
-        return !!doc.getElementById("plasmic-app");
-      }
-      assert(false, "Unreachable code");
-    };
+  const knownRoots = new WeakSet<FiberRoot>();
 
-    const knownRoots = new WeakSet<FiberRoot>();
+  const customHookProps = {
+    onCommitFiberUnmount: (
+      rendererID: any,
+      node: Fiber,
+      ...otherArgs: any[]
+    ) => {
+      rmNode(node);
+      officialHookProps.onCommitFiberUnmount?.(rendererID, node, ...otherArgs);
+    },
+    onCommitFiberRoot: (
+      rendererID: any,
+      fiberRoot: FiberRoot,
+      ...otherArgs: any[]
+    ) => {
+      const { current: rootNode, containerInfo } = fiberRoot;
 
-    const customHookProps = {
-      onCommitFiberUnmount: (
-        rendererID: any,
-        node: Fiber,
-        ...otherArgs: any[]
-      ) => {
-        rmNode(node);
-        officialHookProps.onCommitFiberUnmount?.(rendererID, node, ...otherArgs);
-      },
-      onCommitFiberRoot: (
-        rendererID: any,
-        fiberRoot: FiberRoot,
-        ...otherArgs: any[]
-      ) => {
-        const { current: rootNode, containerInfo } = fiberRoot;
+      // We only need to traverse canvas frames
+      const isCanvas = isCanvasFrame(containerInfo);
+      const isMounted =
+        rootNode.memoizedState != null &&
+        rootNode.memoizedState.element != null;
 
-        // We only need to traverse canvas frames
-        const isCanvas = isCanvasFrame(containerInfo);
-        const isMounted =
-          rootNode.memoizedState != null &&
-          rootNode.memoizedState.element != null;
-
-        if (isCanvas && isMounted) {
-          const wasMounted =
-            rootNode.alternate != null &&
-            rootNode.alternate.memoizedState != null &&
-            rootNode.alternate.memoizedState.element != null;
-          if (!knownRoots.has(fiberRoot) || !wasMounted) {
-            // New tree, traverse it entirely
-            knownRoots.add(fiberRoot);
-            traverseTree(rootNode, (fiber: Fiber) => addNode(fiber), false);
+      if (isCanvas && isMounted) {
+        const wasMounted =
+          rootNode.alternate != null &&
+          rootNode.alternate.memoizedState != null &&
+          rootNode.alternate.memoizedState.element != null;
+        if (!knownRoots.has(fiberRoot) || !wasMounted) {
+          // New tree, traverse it entirely
+          knownRoots.add(fiberRoot);
+          traverseTree(rootNode, (fiber: Fiber) => addNode(fiber), false);
+        } else {
+          if (rootNode.alternate) {
+            traverseUpdates(rootNode, rootNode.alternate, (fiber: Fiber) =>
+              addNode(fiber)
+            );
           } else {
-            if (rootNode.alternate) {
-              traverseUpdates(rootNode, rootNode.alternate, (fiber: Fiber) =>
-                addNode(fiber)
-              );
-            } else {
-              // New tree
-              traverseTree(rootNode, (fiber: Fiber) => addNode(fiber), false);
-            }
+            // New tree
+            traverseTree(rootNode, (fiber: Fiber) => addNode(fiber), false);
           }
         }
-        officialHookProps.onCommitFiberRoot?.(
-          rendererID,
-          fiberRoot,
-          ...otherArgs
-        );
-      },
-    };
-
-    for (const [key, value] of Object.entries(customHookProps)) {
-      // only transfer functions
-      if (typeof value == "function") {
-        officialHook[key] = value;
       }
+      officialHookProps.onCommitFiberRoot?.(
+        rendererID,
+        fiberRoot,
+        ...otherArgs
+      );
+    },
+  };
+
+  for (const [key, value] of Object.entries(customHookProps)) {
+    // only transfer functions
+    if (typeof value == "function") {
+      officialHook[key] = value;
     }
   }
 }
