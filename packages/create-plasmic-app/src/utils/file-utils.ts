1e9486bba5704ffffdc634111a88a0501c932876
Tiago Madeira <tiago@plasmic.app>
2021-07-19 21:30:10 -0300

    [create-plasmic-app] bugs: Move "style" from next/link to <a>
    Clubhouse issue:
    https://app.clubhouse.io/plasmic/story/16715/
    
    Change-Id: I9ac9fc23ba6e9ef3947f8028fa0cebdbc4910cbb

--- packages/create-plasmic-app/src/utils/file-utils.ts
+++ packages/create-plasmic-app/src/utils/file-utils.ts
@@ -308,7 +308,6 @@ function generateWelcomePage(config: any, platform: string): string {
   let hasPages = false;
   let pageComponents: any[];
   let pagesDir: string;
-  const linkTag = platform === "nextjs" ? "Link" : "a";
   if (platform !== "react" && config && L.isArray(config.projects)) {
     pageComponents = L.flatMap(config.projects, (p) => p.components).filter(
       (c) => c.componentType === "page"
@@ -325,7 +324,11 @@ function generateWelcomePage(config: any, platform: string): string {
         const relativePath = path.relative(pagesDir, pc.importSpec.modulePath);
         // Format as an absolute path without the extension name
         const relativeLink = "/" + stripExtension(relativePath);
-        return `<li><${linkTag} style={{ color: "blue" }} href="${relativeLink}">${pc.name} - ${relativeLink}</${linkTag}></li>`;
+        if (platform === "nextjs") {
+          return `<li><Link href="${relativeLink}"><a style={{ color: "blue" }}>${pc.name} - ${relativeLink}</a></Link></li>`;
+        } else {
+          return `<li><a style={{ color: "blue" }} href="${relativeLink}">${pc.name} - ${relativeLink}</a></li>`;
+        }
       })
       .join("\n");
     return `
