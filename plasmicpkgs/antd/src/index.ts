5c4f614b2035f7b148b4ebaee571edd8af289cca
icaro <icaro@plasmic.app>
2021-11-24 12:09:49 -0300

    [feat] add switch component
    Change-Id: Iaeb9ba81ce7a6c337f7c34f334e48d82aa66990b

--- plasmicpkgs/antd/src/index.ts
+++ plasmicpkgs/antd/src/index.ts
@@ -1,3 +1,15 @@
-export { Registerable, registerAll } from "./registerable";
+import { Registerable } from "./registerable";
+import { registerButton } from "./registerButton";
+import { registerSlider } from "./registerSlider";
+import { registerSwitch } from "./registerSwitch";
+
+export { Registerable } from "./registerable";
 export { buttonMeta, registerButton } from "./registerButton";
 export { sliderMeta, registerSlider } from "./registerSlider";
+export { switchMeta, registerSwitch } from "./registerSwitch";
+
+export function registerAll(loader?: Registerable) {
+  registerButton(loader);
+  registerSlider(loader);
+  registerSwitch(loader);
+}
