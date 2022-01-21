19213b1ca3d85e8be19540b55958474280fb7916
Tiago Madeira <tiago@plasmic.app>
2022-01-21 09:23:08 -0300

    [react-web] Forward ref in PlasmicLink
    GitHub issue: https://github.com/plasmicapp/plasmic/issues/27
    
    Change-Id: I2cf589d26bf702cafd79f34b2ba565a7beab64db

--- packages/react-web/src/render/PlasmicLink.tsx
+++ packages/react-web/src/render/PlasmicLink.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { omit, pick } from "../common";
 
-export function PlasmicLink(props: any) {
+export const PlasmicLink = React.forwardRef(function PlasmicLink(
+  props: any,
+  ref: React.Ref<any>
+) {
   // props.href is required for nextjs; if no props.href,
   // then we just render the default anchor element
   if (props.platform === "nextjs" && props.href) {
@@ -18,19 +21,19 @@ export function PlasmicLink(props: any) {
     return React.createElement(
       props.component,
       pick(props, ...nextjsProps),
-      <a {...omit(props, "component", "platform", ...nextjsProps)} />
+      <a {...omit(props, "component", "platform", ...nextjsProps)} ref={ref} />
     );
   }
 
   if (props.platform === "gatsby" && isInternalHref(props.href)) {
     return React.createElement(props.component, {
       ...omit(props, "component", "platform", "href"),
-      ...{ to: props.href },
+      ...{ to: props.href, ref },
     });
   }
 
-  return <a {...omit(props, "component", "platform")} />;
-}
+  return <a {...omit(props, "component", "platform")} ref={ref} />;
+});
 
 function isInternalHref(href: string): boolean {
   return /^\/(?!\/)/.test(href);
