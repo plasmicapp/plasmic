4f1da1d26eb2c94850cf1428d39d99ca4246a0ee
Chung Wu <chungwu@plasmic.app>
2022-02-17 17:50:36 -0800

    Fix bug on checking whether global contexts provider can be loaded
    Was attemping to load "" before
    
    Change-Id: I11d89c1f15b1a11d302baf6dbe5a20461bf21df8

--- packages/loader-react/src/component-lookup.ts
+++ packages/loader-react/src/component-lookup.ts
@@ -39,9 +39,9 @@ export class ComponentLookup {
 
   getGlobalContexts(): { meta: GlobalGroupMeta; context: any }[] {
     const customGlobalMetas = this.bundle.globalGroups.filter(
-      m => m.type === 'global-user-defined'
+      (m) => m.type === 'global-user-defined'
     );
-    return customGlobalMetas.map(meta => ({
+    return customGlobalMetas.map((meta) => ({
       meta,
       context: this.registry.load(meta.contextFile).default,
     }));
@@ -49,12 +49,12 @@ export class ComponentLookup {
 
   getGlobalContextsProvider(projectId: string | undefined) {
     const projectMeta = projectId
-      ? this.bundle.projects.find(x => x.id === projectId)
-      : this.bundle.projects.find(x => !!x.globalContextsProviderFileName);
+      ? this.bundle.projects.find((x) => x.id === projectId)
+      : this.bundle.projects.find((x) => !!x.globalContextsProviderFileName);
     if (
       !projectMeta ||
-      (!!projectMeta.globalContextsProviderFileName &&
-        !this.registry.hasModule(projectMeta.globalContextsProviderFileName))
+      !projectMeta.globalContextsProviderFileName ||
+      !this.registry.hasModule(projectMeta.globalContextsProviderFileName)
     ) {
       return undefined;
     }
@@ -73,11 +73,11 @@ export class ComponentLookup {
   getCss(): AssetModule[] {
     // We can probably always get the modules from the browser build
     return this.bundle.modules.browser.filter(
-      mod => mod.type === 'asset' && mod.fileName.endsWith('css')
+      (mod) => mod.type === 'asset' && mod.fileName.endsWith('css')
     ) as AssetModule[];
   }
 
   getRemoteFonts(): FontMeta[] {
-    return this.bundle.projects.flatMap(p => p.remoteFonts);
+    return this.bundle.projects.flatMap((p) => p.remoteFonts);
   }
 }
