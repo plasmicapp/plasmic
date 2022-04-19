836783a9591520e9c1c293ef1ef56096ca249d5c
Victor Agnez Lima <victor@plasmic.app>
2022-04-19 20:10:56 -0300

    [bug] Fix code component substitution not working w/ imported projects
    Issue: https://app.shortcut.com/plasmic/story/23315
    Change-Id: Ic7e34efc7b1891aebbc18e6008085b97069f27d4

--- packages/loader-react/src/component-lookup.ts
+++ packages/loader-react/src/component-lookup.ts
@@ -1,12 +1,18 @@
 import {
   AssetModule,
+  ComponentMeta,
   FontMeta,
   GlobalGroupMeta,
   LoaderBundleOutput,
   Registry,
 } from '@plasmicapp/loader-core';
 import * as React from 'react';
-import { ComponentLookupSpec, getCompMeta } from './utils';
+import { ComponentLookupSpec, getCompMetas } from './utils';
+
+function getFirstCompMeta(metas: ComponentMeta[], lookup: ComponentLookupSpec) {
+  const filtered = getCompMetas(metas, lookup);
+  return filtered.length === 0 ? undefined : filtered[0];
+}
 
 export class ComponentLookup {
   constructor(private bundle: LoaderBundleOutput, private registry: Registry) {}
@@ -15,7 +21,7 @@ export class ComponentLookup {
     spec: ComponentLookupSpec,
     opts: { forceOriginal?: boolean } = {}
   ) {
-    const compMeta = getCompMeta(this.bundle.components, spec);
+    const compMeta = getFirstCompMeta(this.bundle.components, spec);
     if (!compMeta) {
       throw new Error(`Component not found: ${spec}`);
     }
@@ -30,7 +36,7 @@ export class ComponentLookup {
   }
 
   hasComponent(spec: ComponentLookupSpec) {
-    const compMeta = getCompMeta(this.bundle.components, spec);
+    const compMeta = getFirstCompMeta(this.bundle.components, spec);
     if (compMeta) {
       return this.registry.hasModule(compMeta.entry);
     }
@@ -39,18 +45,18 @@ export class ComponentLookup {
 
   getGlobalContexts(): { meta: GlobalGroupMeta; context: any }[] {
     const customGlobalMetas = this.bundle.globalGroups.filter(
-      m => m.type === 'global-user-defined'
+      (m) => m.type === 'global-user-defined'
     );
-    return customGlobalMetas.map(meta => ({
+    return customGlobalMetas.map((meta) => ({
       meta,
       context: this.registry.load(meta.contextFile).default,
     }));
   }
 
   getGlobalContextsProvider(spec: ComponentLookupSpec) {
-    const compMeta = getCompMeta(this.bundle.components, spec);
+    const compMeta = getFirstCompMeta(this.bundle.components, spec);
     const projectMeta = compMeta
-      ? this.bundle.projects.find(x => x.id === compMeta.projectId)
+      ? this.bundle.projects.find((x) => x.id === compMeta.projectId)
       : undefined;
 
     if (
@@ -75,11 +81,11 @@ export class ComponentLookup {
   getCss(): AssetModule[] {
     // We can probably always get the modules from the browser build
     return this.bundle.modules.browser.filter(
-      mod => mod.type === 'asset' && mod.fileName.endsWith('css')
+      (mod) => mod.type === 'asset' && mod.fileName.endsWith('css')
     ) as AssetModule[];
   }
 
   getRemoteFonts(): FontMeta[] {
-    return this.bundle.projects.flatMap(p => p.remoteFonts);
+    return this.bundle.projects.flatMap((p) => p.remoteFonts);
   }
 }
