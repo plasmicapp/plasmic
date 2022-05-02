51a5634143d85e7990a644ad590bff08a3e5f95e
Mr.Biscuit <sunshuaiqi@gmail.com>
2022-05-02 21:18:44 +0800

    added ant rate component

---
+++ plasmicpkgs/antd/src/registerRate.ts
@@ -0,0 +1,65 @@
+import registerComponent, {
+  ComponentMeta,
+} from "@plasmicapp/host/registerComponent";
+import { Rate, RateProps } from "antd";
+import { Registerable } from "./registerable";
+
+
+export const rateMeta: ComponentMeta<RateProps> = { 
+name: "AntdRate",
+displayName: "Antd Rate",
+props: {
+    allowClear: {
+        type: "boolean",
+        description: "Whether to allow clear when click again",
+        defaultValue: true
+    },
+    allowHalf: {
+        type: "boolean",
+        description: "Whether to allow semi selection",
+        default: false
+    },
+    autoFocus: {
+        type: "boolean",
+        description: "If get focus when component mounted",
+        default: false
+    },
+    count: {
+        type: "number",
+        description: "Star count"
+    },
+    className: {
+        type: "string",
+        description: "The custom class name of rate"
+    },
+    defaultValue: {
+        type: "number",
+        description: "The default value",
+        default: 0
+    },
+    disabled: {
+        type: "boolean",
+        description: "If read only, unable to interact",
+        default: false
+    },
+    tooltips: {
+        type: "array",
+        description: "Customize tooltip by each character",
+    },
+    value: {
+        type: "number",
+        description: "The current value",
+    },
+},
+importPath: "antd",
+importName: "Rate",
+};
+
+export function registerRate(
+  loader?: Registerable,
+  customRateMeta?: ComponentMeta<RateProps>
+) {
+  const doRegisterComponent: typeof registerComponent = (...args) =>
+    loader ? loader.registerComponent(...args) : registerComponent(...args);
+  doRegisterComponent(Rate, customRateMeta ?? rateMeta);
+}
