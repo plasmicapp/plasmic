b42b91a0dcf6356d2547bcadc39685a454aa9fa9
Tiago Madeira <tiago@plasmic.app>
2022-08-17 13:30:41 -0300

    [examples] Update plasmic-cms-* to use @plasmicpkgs/plasmic-cms API
    Change-Id: Ibd5a21d87618d5d5551e6298b99269c35c1c9dc0

--- examples/plasmic-cms-nextjs/pages/[[...catchall]].tsx
+++ examples/plasmic-cms-nextjs/pages/[[...catchall]].tsx
@@ -4,47 +4,22 @@ import {
   PlasmicComponent,
   PlasmicRootProvider,
 } from "@plasmicapp/loader-nextjs";
+import { API } from "@plasmicpkgs/plasmic-cms";
 import type { GetStaticPaths, GetStaticProps } from "next";
 
-import NextError from "next/error";
+import Error from "next/error";
 import { useRouter } from "next/router";
 import { PLASMIC } from "../plasmic-init";
 
-const cmsConfig = {
-  host: `https://studio.plasmic.app`,
-  databaseId: `gCKFKDQ581NNXUi9iwbMyZ`,
-  databaseToken: `69xOVFM7WmWwaLLG1jJhmv9TMvHMH4MXfAkJiuz5dk5Y1IyTW1Z1GzYJVtWfDFZ7nY8ql7FnFaf7T8wNv42WQ`,
-};
-
-async function apiGet(endpoint: string, params: {} = {}) {
-  const url = new URL(
-    `${cmsConfig.host}/api/v1/cms/databases/${cmsConfig.databaseId}${endpoint}`
-  );
-  url.search = new URLSearchParams(params).toString();
-  const response = await fetch(url.toString(), {
-    method: "GET",
-    headers: {
-      accept: "*/*",
-      "x-plasmic-api-cms-tokens": `${cmsConfig.databaseId}:${cmsConfig.databaseToken}`,
-    },
-    mode: "cors",
-  });
-
-  if (response.status !== 200) {
-    const message = await response.text();
-    throw new Error(`${response.status}: ${message}`);
-  }
-
-  return await response.json();
-}
-
 async function getProductSlugs(): Promise<string[]> {
-  const products: { data: { slug: string } }[] = (
-    await apiGet(`/tables/products/query`, {
-      q: "{}",
-    })
-  ).rows;
-  return products.map((p) => p.data.slug);
+  const api = new API({
+    host: `https://studio.plasmic.app`,
+    databaseId: `gCKFKDQ581NNXUi9iwbMyZ`,
+    databaseToken: `69xOVFM7WmWwaLLG1jJhmv9TMvHMH4MXfAkJiuz5dk5Y1IyTW1Z1GzYJVtWfDFZ7nY8ql7FnFaf7T8wNv42WQ`,
+    locale: ``,
+  });
+  const products = await api.query("products");
+  return products.map((p) => p.data?.slug).filter((slug) => slug);
 }
 
 export default function PlasmicLoaderPage(props: {
@@ -54,7 +29,7 @@ export default function PlasmicLoaderPage(props: {
   const { plasmicData, queryCache } = props;
   const router = useRouter();
   if (!plasmicData || plasmicData.entryCompMetas.length === 0) {
-    return <NextError statusCode={404} />;
+    return <Error statusCode={404} />;
   }
   const pageMeta = plasmicData.entryCompMetas[0];
   return (
