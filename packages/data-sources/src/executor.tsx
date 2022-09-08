05f5c9c60066aff676081d0f823b026359497aff
Chung Wu <chungwu@plasmic.app>
2022-09-08 16:10:33 -0700

    [sources] Packages for performing Plasmic data ops
    Change-Id: I9ce8e601c6527f8c8c7f4dbb8f0efabe0f56841d

---
+++ packages/data-sources/src/executor.tsx
@@ -0,0 +1,59 @@
+import fetch from 'isomorphic-unfetch';
+
+const DEFAULT_HOST = 'https://studio.plasmic.app';
+
+export interface DataOp {
+  sourceId: string;
+  opId: string;
+  userArgs?: Record<string, any>;
+}
+
+export async function executePlasmicDataOp<T = any>(
+  op: DataOp,
+  opts?: {
+    userAuthToken?: string;
+  }
+) {
+  const func = getConfig(
+    '__PLASMIC_EXECUTE_DATA_OP',
+    _executePlasmicDataOp
+  ) as typeof _executePlasmicDataOp;
+  return await func<T>(op, opts);
+}
+
+async function _executePlasmicDataOp<T = any>(
+  op: DataOp,
+  opts?: {
+    userAuthToken?: string;
+  }
+) {
+  const host = getConfig('__PLASMIC_DATA_HOST', DEFAULT_HOST);
+
+  const url = `${host}/api/v1/server-data/sources/${op.sourceId}/execute`;
+  const resp = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...(opts?.userAuthToken && {
+        'x-plasmic-data-user-auth-token': opts.userAuthToken,
+      }),
+    },
+    body: JSON.stringify({
+      opId: op.opId,
+      userArgs: op.userArgs ?? {},
+    }),
+  });
+  if (resp.status !== 200) {
+    const text = await resp.text();
+    throw new Error(text);
+  }
+  return (await resp.json()) as T;
+}
+
+function getConfig<T>(key: string, defaultValue: T) {
+  if (typeof window === 'undefined') {
+    return defaultValue;
+  } else {
+    return (window as any)[key] ?? defaultValue;
+  }
+}
---
+++ packages/data-sources/src/index.tsx
@@ -0,0 +1,6 @@
+export { Fetcher } from './components/Fetcher';
+export { DataOp, executePlasmicDataOp } from './executor';
+export {
+  usePlasmicDataMutationOp,
+  usePlasmicDataOp,
+} from './hooks/usePlasmicDataOp';
