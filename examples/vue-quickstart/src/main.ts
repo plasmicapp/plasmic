b53ee235bde27ad8a8a4bc94718fbb05b318f080
Yang Zhang <yang@users.noreply.github.com>
2022-09-09 15:12:27 -0700

    Add Vue quickstart example
    Change-Id: I18019e2d6c5e86cb36c5a058753600334525205a

---
+++ examples/vue-quickstart/src/main.ts
@@ -0,0 +1,14 @@
+import { createPinia } from "pinia";
+import { createApp } from "vue";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "./assets/main.css";
+
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
