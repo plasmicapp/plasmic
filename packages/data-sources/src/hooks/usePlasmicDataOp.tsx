e4399be10577e980516236d13ff5414dba700230
Chung Wu <chungwu@plasmic.app>
2022-10-05 17:50:21 -0700

    [data] Allow fetching paginated data from usePlasmicDataOp()
    Change-Id: Ic2ce7d8a3868b395525a1696ac7f90642c184438

--- packages/data-sources/src/hooks/usePlasmicDataOp.tsx
+++ packages/data-sources/src/hooks/usePlasmicDataOp.tsx
@@ -1,9 +1,15 @@
 import { usePlasmicDataSourceContext } from '@plasmicapp/data-sources-context';
 import { useMutablePlasmicQueryData } from '@plasmicapp/query';
 import React from 'react';
-import { DataOp, executePlasmicDataOp } from '../executor';
+import { DataOp, executePlasmicDataOp, Pagination } from '../executor';
 
-export function usePlasmicDataOp<T = any, E = any>(dataOp: DataOp | undefined) {
+export function usePlasmicDataOp<T = any, E = any>(
+  dataOp: DataOp | undefined,
+  opts?: {
+    includeSchema?: boolean;
+    paginate?: Pagination;
+  }
+) {
   const ctx = usePlasmicDataSourceContext();
   return useMutablePlasmicQueryData<T, E>(
     () =>
@@ -12,17 +18,27 @@ export function usePlasmicDataOp<T = any, E = any>(dataOp: DataOp | undefined) {
             sourceId: dataOp.sourceId,
             opId: dataOp.opId,
             args: dataOp.userArgs,
+            userAuthToken: ctx?.userAuthToken,
+            includeSchema: opts?.includeSchema,
+            paginate: opts?.paginate,
           })
         : null,
     async () => {
       return await executePlasmicDataOp<T>(dataOp!, {
         userAuthToken: ctx?.userAuthToken,
+        includeSchema: opts?.includeSchema,
+        paginate: opts?.paginate,
       });
     }
   );
 }
 
-export function usePlasmicDataMutationOp<T = any>(dataOp: DataOp | undefined) {
+export function usePlasmicDataMutationOp<T = any>(
+  dataOp: DataOp | undefined,
+  opts?: {
+    includeSchema?: boolean;
+  }
+) {
   const { sourceId, opId, userArgs } = dataOp ?? {};
   const ctx = usePlasmicDataSourceContext();
   const userToken = ctx?.userAuthToken;
@@ -34,6 +50,7 @@ export function usePlasmicDataMutationOp<T = any>(dataOp: DataOp | undefined) {
       { sourceId, opId, userArgs },
       {
         userAuthToken: userToken,
+        includeSchema: opts?.includeSchema,
       }
     );
   }, [sourceId, opId, userArgs, userToken]);
