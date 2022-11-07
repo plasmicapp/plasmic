920417622a0ac3450e33cdb3060644200baa43bc
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-11-07 15:54:02 +0500

    Mailchimp Integration
    Change-Id: I69ff6715d2e9c1dd32d42d438d9f5d8d5f429136

---
+++ plasmicpkgs/mailchimp/src/mailchimp.tsx
@@ -0,0 +1,62 @@
+import { ComponentMeta } from "@plasmicapp/host";
+
+import React from "react";
+
+export function ensure<T>(x: T | null | undefined): T {
+  if (x === null || x === undefined) {
+    debugger;
+    throw new Error(`Value must not be undefined or null`);
+  } else {
+    return x;
+  }
+}
+
+const modulePath = "@plasmicpkgs/plasmic-mailchimp";
+
+interface MailchimpSignupFormProps {
+  className?: string;
+  url?: string;
+}
+
+export const MailchimpSignupFormMeta: ComponentMeta<MailchimpSignupFormProps> =
+{
+  name: "MailchimpSignupForm",
+  displayName: "Mailchimp Signup Form",
+  importName: "MailchimpSignupForm",
+  importPath: modulePath,
+  providesData: true,
+  description:
+    "Shows a sign up form to users for subscribe to your newsletter",
+  props: {
+    url: {
+      type: "string",
+      displayName: "URL",
+      description: `Learn how to get your form url ("https://mailchimp.com/help/host-your-own-signup-forms/")`,
+      defaultValue: "http://eepurl.com/ic43yL",
+      helpText:'You can learn how.(https://mailchimp.com/help/share-your-signup-form/)'
+      
+    },
+  },
+};
+
+export function MailchimpSignupForm({
+  className,
+  url,
+}: MailchimpSignupFormProps) {
+  if (!url) {
+    return <div>Please specify URL</div>;
+  }
+
+  return (
+    <iframe
+      src={url}
+      width="100%"
+      frameBorder="0"
+      marginHeight={0}
+      scrolling="no"
+      marginWidth={0}
+      height="600px"
+      className={className}
+    />
+  );
+}
