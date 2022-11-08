bc817e7bdba9015258fb9555ba5992664a263e42
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-11-08 12:02:42 +0500

    Eventbrite Integration
    Change-Id: I18ac1a5c2a820dc1381d8a69fe4a8e9121c22d43

---
+++ plasmicpkgs/eventbrite/src/eventbrite.tsx
@@ -0,0 +1,78 @@
+import { ComponentMeta } from "@plasmicapp/host";
+
+import React from "react";
+import useEventbrite from "./hooks/useEventBrite";
+
+export function ensure<T>(x: T | null | undefined): T {
+  if (x === null || x === undefined) {
+    debugger;
+    throw new Error(`Value must not be undefined or null`);
+  } else {
+    return x;
+  }
+}
+
+const modulePath = "@plasmicpkgs/plasmic-eventbrite";
+
+interface EventbriteProps {
+  className?: string;
+  eventId?: string;
+  text?: string;
+}
+
+export const EventbriteMeta: ComponentMeta<EventbriteProps> = {
+  name: "Eventbrite",
+  displayName: "EventBrite",
+  importName: "Eventbrite",
+  importPath: modulePath,
+  providesData: true,
+  description: "Shows Eventbrite checkout on your website",
+  defaultStyles: {
+    color: "#ffffff",
+    fontSize: "12px",
+    width: "100px",
+    height: "25px",
+    borderWidth: "0px",
+    backgroundColor: "#19aee7",
+    borderRadius: "4px",
+  },
+  props: {
+    eventId: {
+      type: "string",
+      displayName: "Event ID",
+      description: `Learn how to get event ID ("https://www.eventbrite.com/platform/docs/events")`,
+      defaultValue: "463676879027",
+      helpText:
+        "In the URL field at the top, you see something like https://www.eventbrite.com/myevent?eid=123456789 . The number after eid= is the Event ID",
+    },
+    text: {
+      type: "string",
+      displayName: "Label",
+      description: "Label",
+      defaultValue: "Buy tickets",
+    },
+  },
+};
+
+export function Eventbrite({ className, text, eventId }: EventbriteProps) {
+  
+  if (!eventId) {
+    return <div>Please enter Event Id</div>;
+  }
+
+  const handleOrderCompleted = React.useCallback(() => {
+    console.log("Order was completed successfully");
+  }, []);
+
+  const modalButtonCheckout = useEventbrite({
+    eventId: `${eventId!}`,
+    modal: true,
+    onOrderComplete: handleOrderCompleted,
+  });
+
+  return (
+    <button className={className} id={modalButtonCheckout?.id} type="button">
+      {text}
+    </button>
+  );
+}
---
+++ plasmicpkgs/eventbrite/src/hooks/useEventBrite.ts
@@ -0,0 +1,99 @@
+import React from "react";
+import { v4 as uuid } from "uuid";
+
+const TAG_ID = `EB_SCRIPT_${uuid()}`;
+const SCRIPT_URL = "https://www.eventbrite.com/static/widgets/eb_widgets.js";
+
+interface Props {
+    eventId?: string;
+    modal?: boolean;
+    onOrderComplete: () => void;
+    promoCode?: string;
+    iFrameHeight?: string;
+    iFrameAutoAdapt?: string;
+}
+type Widget = {
+    widgetType?: string;
+    eventId?: string;
+    onOrderComplete: () => void;
+    modal?: boolean;
+    modalTriggerElementId?: string;
+    promoCode?: string;
+    iframeContainerId?: string;
+}
+const useEventbrite = ({
+    eventId,
+    modal,
+    onOrderComplete,
+    promoCode,
+}: Props) => {
+    const id = `EB_${uuid()}`;
+    const [isLoaded, setLoaded] = React.useState(false);
+    const onLoad = React.useCallback(() => setLoaded(true), [setLoaded]);
+    const onErr = React.useCallback(
+        (e) => {
+            console.error(`Failed to load Eventbrite script from ${SCRIPT_URL}`);
+            console.error(e);
+
+            setLoaded(false);
+        },
+        [setLoaded]
+    );
+
+    React.useEffect(() => {
+        //@ts-ignore
+        if (globalThis.window?.EBWidgets) {
+            setLoaded(true);
+            return;
+        }
+        const existing = document.getElementById(TAG_ID);
+        if (existing) {
+            existing.remove();
+        }
+        const script = document.createElement("script");
+        script.id = TAG_ID;
+        script.async = true;
+        script.src = SCRIPT_URL;
+        script.addEventListener("load", onLoad);
+        script.addEventListener("error", onErr);
+        script.addEventListener("abort", onErr);
+        document.head.appendChild(script);
+
+        return () => {
+            script.removeEventListener("load", onLoad);
+            script.removeEventListener("error", onErr);
+            script.removeEventListener("abort", onErr);
+            script.remove();
+            setLoaded(false);
+        };
+    }, [setLoaded, onLoad, onErr]);
+
+    React.useEffect(() => {
+        if (!isLoaded) {
+            return;
+        }
+
+        const config: Widget = {
+            widgetType: "checkout",
+            eventId,
+            onOrderComplete,
+            modal,
+        };
+
+        if (modal) {
+            config.modalTriggerElementId = id;
+        } else {
+            config.iframeContainerId = id;
+        }
+
+        if (promoCode) {
+            config.promoCode = promoCode;
+        }
+        //@ts-ignore
+        globalThis.window?.EBWidgets.createWidget(config);
+    }, [isLoaded]);
+
+    return isLoaded ? { id } : null;
+};
+
+export default useEventbrite;
