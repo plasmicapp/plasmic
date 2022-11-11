9303f954275eea8da81646b6f61c480b51a14616
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-11-11 23:18:39 +0500

    SoundCloud Integration
    Change-Id: I5a36c5ef293478d1a0179661eb1a23809cb37f06

---
+++ plasmicpkgs/soundcloud/src/index.tsx
@@ -0,0 +1,31 @@
+import registerComponent, {
+  ComponentMeta,
+} from "@plasmicapp/host/registerComponent";
+import registerGlobalContext from "@plasmicapp/host/registerGlobalContext";
+import {
+  SoundCloud,
+  SoundCloudMeta,
+} from "./soundcloud";
+
+
+export function registerAll(loader?: {
+  registerComponent: typeof registerComponent;
+  registerGlobalContext: typeof registerGlobalContext;
+}) {
+  const _registerComponent = <T extends React.ComponentType<any>>(
+    Component: T,
+    defaultMeta: ComponentMeta<React.ComponentProps<T>>
+  ) => {
+    if (loader) {
+      loader.registerComponent(Component, defaultMeta);
+    } else {
+      registerComponent(Component, defaultMeta);
+    }
+  };
+
+  if (loader) {
+    _registerComponent(SoundCloud, SoundCloudMeta);
+  }
+}
+
+export * from "./soundcloud";
