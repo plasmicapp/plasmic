1dc0cec29250bccef57fc2368af68c28b0fb4684
abdukhamiddev <latipovabdukhamid@gmail.com>
2022-11-11 23:58:43 +0500

    Spotify Integration
    Change-Id: Ib6774f0e5caa48dcbc9a30e10c08824ab6ee778a

---
+++ plasmicpkgs/spotify/src/index.tsx
@@ -0,0 +1,31 @@
+import registerComponent, {
+  ComponentMeta,
+} from "@plasmicapp/host/registerComponent";
+import registerGlobalContext from "@plasmicapp/host/registerGlobalContext";
+import {
+  Spotify,
+  SpotifyMeta,
+} from "./spotify";
+
+
+export function registerAll(loader?: {
+  registerComponent: typeof registerComponent;
+  registerGlobalContext: typeof registerGlobalContext;
+}) {
+  const _registerComponent = <T extends React.ComponentType<any>>(
+    Component: T,
+    defaultMeta: ComponentMeta<React.ComponentProps<T>>
+  ) => {
+    if (loader) {
+      loader.registerComponent(Component, defaultMeta);
+    } else {
+      registerComponent(Component, defaultMeta);
+    }
+  };
+
+  if (loader) {
+    _registerComponent(Spotify, SpotifyMeta);
+  }
+}
+
+export * from "./spotify";
---
+++ plasmicpkgs/spotify/src/spotify.tsx
@@ -0,0 +1,62 @@
+import { ComponentMeta } from "@plasmicapp/host";
+
+import React from "react";
+export function ensure<T>(x: T | null | undefined): T {
+  if (x === null || x === undefined) {
+    debugger;
+    throw new Error(`Value must not be undefined or null`);
+  } else {
+    return x;
+  }
+}
+
+const modulePath = "@plasmicpkgs/plasmic-spotify";
+
+interface SpotifyProps {
+  theme?: boolean;
+  url?: string;
+  className?: string;
+}
+
+export const SpotifyMeta: ComponentMeta<SpotifyProps> = {
+  name: "Spotify",
+  displayName: "Spotify",
+  importName: "Spotify",
+  importPath: modulePath,
+  providesData: true,
+  description: "Spotify Player",
+  props: {
+    url: {
+      type: "string",
+      displayName: "URL",
+      description: "Song URL",
+      defaultValue:
+        "https://open.spotify.com/embed/track/3rmo8F54jFF8OgYsqTxm5d",
+    },
+    theme: {
+      type: "boolean",
+      displayName: "Theme",
+      description: "Theme",
+    },
+
+  },
+};
+
+export function Spotify({
+  className,
+  url,
+  theme,
+}: SpotifyProps) {
+  const value = theme ? "1" : "0"
+  const query = `${url}?utm_source=generator&theme=${value}`;
+  return (
+    <iframe
+      src={query}
+      frameBorder="0"
+      scrolling="no"
+      width="400"
+      height="166"
+      className={className}
+    />
+  );
+}
