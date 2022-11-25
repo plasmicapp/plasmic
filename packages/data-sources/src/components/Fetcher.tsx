e70b288f009bf75284383e69a2888a083df804c7
Victor Agnez Lima <victor@plasmic.app>
2022-11-25 17:14:51 -0300

    [data] Update Fetcher component interface
    Issue: https://app.shortcut.com/plasmic/story/28917
    Change-Id: Ifa8593a84e65660a3dcb4ce00cbbcbb104a7939f

--- packages/data-sources/src/components/Fetcher.tsx
+++ packages/data-sources/src/components/Fetcher.tsx
@@ -1,16 +1,19 @@
-import { DataProvider } from '@plasmicapp/host';
+import { ComponentMeta } from '@plasmicapp/host';
 import React from 'react';
 import { DataOp } from '../executor';
 import { usePlasmicDataOp } from '../hooks/usePlasmicDataOp';
 
-export function Fetcher(props: {
+export interface FetcherProps {
   dataOp?: DataOp;
   name?: string;
-  children?: React.ReactNode;
+  children?: ($queries: Record<string, any>) => React.ReactElement | null;
+  queries?: Record<string, any>;
   includeSchema?: boolean;
   pageIndex?: number;
   pageSize?: number;
-}) {
+}
+
+export function Fetcher(props: FetcherProps): React.ReactElement | null {
   const { dataOp, children, name, includeSchema, pageIndex, pageSize } = props;
   const { data, error } = usePlasmicDataOp(dataOp, {
     includeSchema,
@@ -19,13 +22,52 @@ export function Fetcher(props: {
         paginate: { pageIndex, pageSize },
       }),
   });
-  return (
-    <DataProvider name={name ?? 'data'} data={data}>
-      {error || data?.error ? (
-        <div>Error: {error?.toString() || data?.error?.message}</div>
-      ) : (
-        children
-      )}
-    </DataProvider>
+
+  const $queries = React.useMemo(
+    () => ({ ...props.queries, [name ?? 'data']: data }),
+    [props.queries, name, data]
+  );
+
+  return error || data?.error ? (
+    <div>Error: {error?.toString() || data?.error?.message}</div>
+  ) : (
+    children?.($queries) ?? null
   );
 }
+
+export const FetcherMeta: ComponentMeta<FetcherProps> = {
+  name: 'plasmic-data-source-fetcher',
+  displayName: 'Data Source Fetcher',
+  props: {
+    dataOp: {
+      type: 'dataSourceOp' as any,
+      displayName: 'Data',
+    },
+    name: {
+      type: 'string',
+      displayName: 'Variable name',
+    },
+    children: 'slot',
+    includeSchema: {
+      type: 'boolean',
+      advanced: true,
+      displayName: 'Include data schema',
+      description: 'Includes the data schema in the payload',
+    },
+    pageSize: {
+      type: 'number',
+      advanced: true,
+      displayName: 'Page size',
+      description: 'Only fetch in batches of this size; for pagination',
+    },
+    pageIndex: {
+      type: 'number',
+      advanced: true,
+      displayName: 'Page index',
+      description: '0-based index of the paginated page to fetch',
+    },
+  },
+  importPath: '@plasmicapp/react-web/lib/data-sources',
+  importName: 'Fetcher',
+  styleSections: false,
+};
