b2786a236233d97037ab06caeabcb13b30072874
Jason Long <j@jaslong.com>
2022-12-21 16:23:00 -0800

    Next.js 12 example
    Change-Id: I89080c160a43fecd0a1cf1c7552094bd067e1498

---
+++ examples/nextjs12-example/pages/plasmic-host.tsx
@@ -0,0 +1,6 @@
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "../plasmic-init";
+
+export default function PlasmicHost() {
+  return PLASMIC && <PlasmicCanvasHost />;
+}
