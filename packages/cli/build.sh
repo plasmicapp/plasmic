281d238e3947e10a548b258f76ed0d97122a15a6
Chung Wu <chungwu@plasmic.app>
2023-01-09 14:55:10 -0800

    [cli] Fix eslint not found error...?
    Change-Id: Icc3d7533e89508364bf7a83a64491af1a0bb1de1

--- packages/cli/build.sh
+++ packages/cli/build.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-yarn eslint 'src/**'
+./node_modules/.bin/eslint 'src/**'
 tsc
 cp ./tsconfig-transform.json ./dist
 typescript-json-schema tsconfig.json 'PlasmicConfig' --excludePrivate --required > ./dist/plasmic.schema.json
