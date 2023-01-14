767d291c52a98e648eef76863a7ddfbe036a0768
Samuel <samuel@plasmic.app>
2023-01-14 12:35:51 -0300

    [state-management] fix case when initial object is undefined
    Change-Id: I2db32ea6d50cd111a5443d9fb381c63c8bf00b8c

--- packages/react-web/src/states/valtio.tsx
+++ packages/react-web/src/states/valtio.tsx
@@ -229,7 +229,7 @@ function create$StateProxy(
           : {}
         : Array.isArray(initialObject)
         ? []
-        : Object.create(Object.getPrototypeOf(initialObject));
+        : Object.create(Object.getPrototypeOf(initialObject ?? {}));
     const proxyObj = new Proxy(baseObject, handlers);
     if (initialObject) {
       Reflect.ownKeys(initialObject).forEach((key) => {
@@ -274,56 +274,53 @@ export function useDollarState(
   $$state.props = props;
   $$state.ctx = $ctx ?? {};
   const $state = React.useRef(
-    (() => {
-      const useRef$state = Object.assign(
-        create$StateProxy($$state, (node, path, proxyRoot) => {
-          if (!node.hasState(path)) {
-            node.createStateCell(path);
-            const spec = node.getSpec();
-            if (spec.initFunc) {
-              initializeStateValue($$state, node, path, proxyRoot);
-            } else if (!spec.valueProp) {
-              set(proxyRoot, path, spec.initVal);
-            }
+    Object.assign(
+      create$StateProxy($$state, (node, path, proxyRoot) => {
+        if (!node.hasState(path)) {
+          node.createStateCell(path);
+          const spec = node.getSpec();
+          if (spec.initFunc) {
+            initializeStateValue($$state, node, path, proxyRoot);
+          } else if (!spec.valueProp) {
+            set(proxyRoot, path, spec.initVal);
           }
-          return {
-            get(target, property, receiver) {
-              const spec = node.getSpec();
-              if (spec.valueProp) {
-                return $$state.props[spec.valueProp];
-              } else {
-                return Reflect.get(target, property, receiver);
-              }
-            },
-          };
-        }),
-        {
-          registerInitFunc: function <T>(
-            pathStr: string,
-            f: InitFunc<T>,
-            repetitionIndex?: number[]
-          ) {
-            const { node, realPath } = findStateCell(
-              $$state.rootSpecTree,
-              pathStr,
-              repetitionIndex
-            );
-            if (!node.hasState(realPath)) {
-              node.createStateCell(realPath);
-            }
-            if (
-              !deepEqual(
-                node.getState(realPath).initialValue,
-                f($$state.props, useRef$state, $$state.ctx)
-              )
-            ) {
-              $$state.registrationsQueue.push({ node, path: realPath, f });
+        }
+        return {
+          get(target, property, receiver) {
+            const spec = node.getSpec();
+            if (spec.valueProp) {
+              return $$state.props[spec.valueProp];
+            } else {
+              return Reflect.get(target, property, receiver);
             }
           },
-        }
-      );
-      return useRef$state;
-    })()
+        };
+      }),
+      {
+        registerInitFunc: function <T>(
+          pathStr: string,
+          f: InitFunc<T>,
+          repetitionIndex?: number[]
+        ) {
+          const { node, realPath } = findStateCell(
+            $$state.rootSpecTree,
+            pathStr,
+            repetitionIndex
+          );
+          if (!node.hasState(realPath)) {
+            node.createStateCell(realPath);
+          }
+          if (
+            !deepEqual(
+              node.getState(realPath).initialValue,
+              f($$state.props, $state, $$state.ctx)
+            )
+          ) {
+            $$state.registrationsQueue.push({ node, path: realPath, f });
+          }
+        },
+      }
+    )
   ).current;
 
   // For each spec with an initFunc, evaluate it and see if
