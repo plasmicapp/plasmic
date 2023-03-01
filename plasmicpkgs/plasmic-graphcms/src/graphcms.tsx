2cf1aa96c1985eb0646e8520287b0d193ed13db5
abdukhamiddev <latipovabdukhamid@gmail.com>
2023-03-01 05:23:55 +0500

    noAutoRepeat props for GraphCMS
    Change-Id: I9334d365ea19d5f761d610156fe1c31144be7ef5

--- plasmicpkgs/plasmic-graphcms/src/graphcms.tsx
+++ plasmicpkgs/plasmic-graphcms/src/graphcms.tsx
@@ -25,34 +25,34 @@ interface GraphCMSCredentialsProviderProps {
   authToken: string;
 }
 
-const CredentialsContext = React.createContext<
-  GraphCMSCredentialsProviderProps | undefined
->(undefined);
+const CredentialsContext =
+  React.createContext<GraphCMSCredentialsProviderProps | undefined>(undefined);
 
-export const GraphCMSCredentialsProviderMeta: GlobalContextMeta<GraphCMSCredentialsProviderProps> = {
-  name: "GraphCMSCredentialsProvider",
-  displayName: "GraphCMS Credentials Provider",
-  description:
-    "Permanent Auth Tokens are used for controlling access to querying, mutating content, and comes in the form of Bearer token authentication.[get your Auth Token](https://graphcms.com/docs/api-reference/basics/authorization#permanent-auth-tokens).",
-  importName: "GraphCMSCredentialsProvider",
-  importPath: modulePath,
-  props: {
-    apiUrl: {
-      type: "string",
-      displayName: "API url",
-      description: "API url of your GraphCMS ",
-      defaultValue:
-        "https://api-us-west-2.graphcms.com/v2/cl3ua8gpwdni001z10ucc482i/master",
+export const GraphCMSCredentialsProviderMeta: GlobalContextMeta<GraphCMSCredentialsProviderProps> =
+  {
+    name: "GraphCMSCredentialsProvider",
+    displayName: "GraphCMS Credentials Provider",
+    description:
+      "Permanent Auth Tokens are used for controlling access to querying, mutating content, and comes in the form of Bearer token authentication.[get your Auth Token](https://graphcms.com/docs/api-reference/basics/authorization#permanent-auth-tokens).",
+    importName: "GraphCMSCredentialsProvider",
+    importPath: modulePath,
+    props: {
+      apiUrl: {
+        type: "string",
+        displayName: "API url",
+        description: "API url of your GraphCMS ",
+        defaultValue:
+          "https://api-us-west-2.graphcms.com/v2/cl3ua8gpwdni001z10ucc482i/master",
+      },
+      authToken: {
+        type: "string",
+        displayName: "Auth Token ",
+        description: "Auth Token",
+        defaultValue:
+          "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NTQwMDg5NzUsImF1ZCI6WyJodHRwczovL2FwaS11cy13ZXN0LTIuZ3JhcGhjbXMuY29tL3YyL2NsM3VhOGdwd2RuaTAwMXoxMHVjYzQ4MmkvbWFzdGVyIiwiaHR0cHM6Ly9tYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiMWU2NGY0ZDMtODE3Yy00OTdkLWE4YTQtNzA4OTY4Zjg3OTc3IiwianRpIjoiY2thNWoyZW9iMDN0YzAxd2gwZGZkNjdyeSJ9.bWr3rpqT7UmJ5NwoEVatkW_QsqxC8tB-zxdcTecIVR19oS5tcoxbbmwe946B-57Zmqrnc5rNntj9UjN065RqEDFM0iPhy4BCgDHCFfNUuHg5Mmq1qu8-j_ZSN90aJfwVmMNYH9GuOYFiOCd6uytLe8fPcQRWOKpXEcO8q4BusrreCvwkwXIaZV2dq-FOJ4LdBdKcRWwfQWeMdthVzBxrlrxogP_xEYQuMNdfbe5tGWgVsRVDN7eQjB1w9Srqc9T_NgY6x-aL8rPmobcZ1IMdUj9klPPm_dINMzrhZS4OR-HXHPwdnSFObgPeJDPI6YEo2SFAg78PMCNZNRT2DtfDVC4F7cLboxaNUNY4r6Z2d9uBu2N1o05zIXra6Q4JIA--0xBfELTUcmU06Ococioyui8PCI5r_QlRSSlnxrdb85Ht00yMDBRGHPtySGUNiEy9Lq5RcoW1a41bJRmZ-z1Q8zluOUHrgwcIb2DN8xKB9YThPce7ytnFcVajH0K3Hnd57m7SukCgZACmULt_EK0NYTUe1BBmTC8eg9ZBM3lplPWSUzBKWgajGTUNK50KRWokAke_UCEf0gssR3MYLIo5PVN131-bD57nccEPBkegYmmZUACRoYHyI_gQYC-0---MXCS6BV7cK1D-_yDbiBrCCixyKNBYCmhxuZOxVcWu4dk",
+      },
     },
-    authToken: {
-      type: "string",
-      displayName: "Auth Token ",
-      description: "Auth Token",
-      defaultValue:
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImdjbXMtbWFpbi1wcm9kdWN0aW9uIn0.eyJ2ZXJzaW9uIjozLCJpYXQiOjE2NTQwMDg5NzUsImF1ZCI6WyJodHRwczovL2FwaS11cy13ZXN0LTIuZ3JhcGhjbXMuY29tL3YyL2NsM3VhOGdwd2RuaTAwMXoxMHVjYzQ4MmkvbWFzdGVyIiwiaHR0cHM6Ly9tYW5hZ2VtZW50LW5leHQuZ3JhcGhjbXMuY29tIl0sImlzcyI6Imh0dHBzOi8vbWFuYWdlbWVudC5ncmFwaGNtcy5jb20vIiwic3ViIjoiMWU2NGY0ZDMtODE3Yy00OTdkLWE4YTQtNzA4OTY4Zjg3OTc3IiwianRpIjoiY2thNWoyZW9iMDN0YzAxd2gwZGZkNjdyeSJ9.bWr3rpqT7UmJ5NwoEVatkW_QsqxC8tB-zxdcTecIVR19oS5tcoxbbmwe946B-57Zmqrnc5rNntj9UjN065RqEDFM0iPhy4BCgDHCFfNUuHg5Mmq1qu8-j_ZSN90aJfwVmMNYH9GuOYFiOCd6uytLe8fPcQRWOKpXEcO8q4BusrreCvwkwXIaZV2dq-FOJ4LdBdKcRWwfQWeMdthVzBxrlrxogP_xEYQuMNdfbe5tGWgVsRVDN7eQjB1w9Srqc9T_NgY6x-aL8rPmobcZ1IMdUj9klPPm_dINMzrhZS4OR-HXHPwdnSFObgPeJDPI6YEo2SFAg78PMCNZNRT2DtfDVC4F7cLboxaNUNY4r6Z2d9uBu2N1o05zIXra6Q4JIA--0xBfELTUcmU06Ococioyui8PCI5r_QlRSSlnxrdb85Ht00yMDBRGHPtySGUNiEy9Lq5RcoW1a41bJRmZ-z1Q8zluOUHrgwcIb2DN8xKB9YThPce7ytnFcVajH0K3Hnd57m7SukCgZACmULt_EK0NYTUe1BBmTC8eg9ZBM3lplPWSUzBKWgajGTUNK50KRWokAke_UCEf0gssR3MYLIo5PVN131-bD57nccEPBkegYmmZUACRoYHyI_gQYC-0---MXCS6BV7cK1D-_yDbiBrCCixyKNBYCmhxuZOxVcWu4dk",
-    },
-  },
-};
+  };
 
 export function GraphCMSCredentialsProvider({
   apiUrl,
@@ -69,6 +69,7 @@ export function GraphCMSCredentialsProvider({
 interface GraphCMSFetcherProps {
   children?: ReactNode;
   className?: string;
+  noAutoRepeat?: boolean;
   noLayout?: boolean;
   query?: string;
   setControlContextData?: (data: {
@@ -113,6 +114,12 @@ export const GraphCMSFetcherMeta: ComponentMeta<GraphCMSFetcherProps> = {
       endpoint: (props, ctx) => ctx?.endpoint ?? "",
       headers: (props, ctx) => ctx?.headers ?? "",
     },
+    noAutoRepeat: {
+      type: "boolean",
+      displayName: "No auto-repeat",
+      description: "Do not automatically repeat children for every entry.",
+      defaultValue: false,
+    },
     noLayout: {
       type: "boolean",
       displayName: "No layout",
@@ -132,6 +139,7 @@ export function GraphCMSFetcher({
   children,
   className,
   noLayout,
+  noAutoRepeat,
   setControlContextData,
 }: GraphCMSFetcherProps) {
   const creds = ensure(useContext(CredentialsContext));
@@ -139,6 +147,7 @@ export function GraphCMSFetcher({
     query,
     creds,
   });
+  
   const headers = {
     Authorization: `Bearer ${creds.authToken}`,
   };
@@ -187,17 +196,19 @@ export function GraphCMSFetcher({
     return <div>Data not found</div>;
   }
 
-  const renderedData = Object.values(data?.data)
-    .flatMap((model: any) => (Array.isArray(model) ? model : [model]))
-    .map((item: any, i: number) => (
-      <DataProvider
-        key={JSON.stringify(item)}
-        name={"graphCmsItem"}
-        data={item}
-      >
-        {repeatedElement(i, children)}
-      </DataProvider>
-    ));
+  const renderedData = noAutoRepeat
+    ? children
+    : Object.values(data?.data)
+        .flatMap((model: any) => (Array.isArray(model) ? model : [model]))
+        .map((item: any, i: number) => (
+          <DataProvider
+            key={JSON.stringify(item)}
+            name={"graphCmsItem"}
+            data={item}
+          >
+            {repeatedElement(i, children)}
+          </DataProvider>
+        ));
   return noLayout ? (
     <> {renderedData} </>
   ) : (
