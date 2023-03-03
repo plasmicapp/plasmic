19541a31eed86a86466cb3ed5b53ad92a77e2fb1
Yang Zhang <yang@users.noreply.github.com>
2023-03-02 18:28:23 -0800

    Make CMS code components less confusing
    Change-Id: I4508b8f117021cb90f3e31861dfeedc993175ebf

--- plasmicpkgs/plasmic-cms/src/components.tsx
+++ plasmicpkgs/plasmic-cms/src/components.tsx
@@ -63,44 +63,45 @@ interface CmsCredentialsProviderProps extends DatabaseConfig {
 
 const defaultHost = "https://studio.plasmic.app";
 
-export const cmsCredentialsProviderMeta: GlobalContextMeta<CmsCredentialsProviderProps> = {
-  name: `${componentPrefix}-credentials-provider`,
-  displayName: "CMS Credentials Provider",
-  description: `
+export const cmsCredentialsProviderMeta: GlobalContextMeta<CmsCredentialsProviderProps> =
+  {
+    name: `${componentPrefix}-credentials-provider`,
+    displayName: "CMS Credentials Provider",
+    description: `
 Find your CMS in the [dashboard](https://studio.plasmic.app), and go to the Settings tab for the ID and token.
 
 See also the [getting started video](https://www.youtube.com/watch?v=-Rrn92VtRBc).`,
-  importName: "CmsCredentialsProvider",
-  importPath: modulePath,
-  providesData: true,
-  props: {
-    host: {
-      type: "string",
-      displayName: "Studio URL",
-      description: `The default host for use in production is ${defaultHost}.`,
-      defaultValue: defaultHost,
-      defaultValueHint: defaultHost,
+    importName: "CmsCredentialsProvider",
+    importPath: modulePath,
+    providesData: true,
+    props: {
+      host: {
+        type: "string",
+        displayName: "Studio URL",
+        description: `The default host for use in production is ${defaultHost}.`,
+        defaultValue: defaultHost,
+        defaultValueHint: defaultHost,
+      },
+      databaseId: {
+        type: "string",
+        displayName: "CMS ID",
+        description:
+          "The ID of the CMS (database) to use. (Can get on the CMS settings page)",
+      },
+      databaseToken: {
+        type: "string",
+        displayName: "CMS Public Token",
+        description:
+          "The Public Token of the CMS (database) you are using. (Can get on the CMS settings page)",
+      },
+      locale: {
+        type: "string",
+        displayName: "Locale",
+        description:
+          "The locale to use for localized values, leave empty for the default locale.",
+      },
     },
-    databaseId: {
-      type: "string",
-      displayName: "CMS ID",
-      description:
-        "The ID of the CMS (database) to use. (Can get on the CMS settings page)",
-    },
-    databaseToken: {
-      type: "string",
-      displayName: "CMS Public Token",
-      description:
-        "The Public Token of the CMS (database) you are using. (Can get on the CMS settings page)",
-    },
-    locale: {
-      type: "string",
-      displayName: "Locale",
-      description:
-        "The locale to use for localized values, leave empty for the default locale.",
-    },
-  },
-};
+  };
 
 export function CmsCredentialsProvider({
   children,
@@ -415,7 +416,11 @@ export const cmsRowFieldMeta: ComponentMeta<CmsRowFieldProps> = {
     table: {
       type: "choice",
       displayName: "Model",
-      description: "CMS model (table) to use.",
+      hidden: (props, ctx: TableContextData | null) =>
+        (ctx?.tables?.length ?? 0) <= 1 && !props.table,
+      helpText: "Pick model from a CMS Data Loader",
+      description:
+        "Usually not used! Only with multiple CMS Data Loaders, use this to choose which to show. Otherwise, go select the CMS Data Loader if you want to load different data.",
       options: (_, ctx) => mkTableOptions(ctx?.tables),
       defaultValueHint: (_, ctx) => ctx?.table,
     },
@@ -670,7 +675,11 @@ export const cmsRowLinkMeta: ComponentMeta<CmsRowLinkProps> = {
     table: {
       type: "choice",
       displayName: "Model",
-      description: "CMS model (table) to use.",
+      hidden: (props, ctx: TableContextData | null) =>
+        (ctx?.tables?.length ?? 0) <= 1 && !props.table,
+      helpText: "Pick model from a CMS Data Loader",
+      description:
+        "Usually not used! Only with multiple CMS Data Loaders, use this to choose which to show. Otherwise, go select the CMS Data Loader if you want to load different data.",
       options: (_: any, ctx: TableContextData | null) =>
         mkTableOptions(ctx?.tables),
       defaultValueHint: (_, ctx) => ctx?.table,
@@ -786,7 +795,11 @@ export const cmsRowImageMeta: ComponentMeta<CmsRowImageProps> = {
     table: {
       type: "choice",
       displayName: "Model",
-      description: "CMS model (table) to use.",
+      hidden: (props, ctx: TableContextData | null) =>
+        (ctx?.tables?.length ?? 0) <= 1 && !props.table,
+      helpText: "Pick model from a CMS Data Loader",
+      description:
+        "Usually not used! Only with multiple CMS Data Loaders, use this to choose which to show. Otherwise, go select the CMS Data Loader if you want to load different data.",
       options: (_: any, ctx: TableContextData | null) =>
         mkTableOptions(ctx?.tables),
       defaultValueHint: (_, ctx) => ctx?.table,
@@ -883,7 +896,11 @@ export const cmsRowFieldValueMeta: ComponentMeta<CmsRowFieldValueProps> = {
     table: {
       type: "choice",
       displayName: "Model",
-      description: "CMS model (table) to use.",
+      hidden: (props, ctx: TableContextData | null) =>
+        (ctx?.tables?.length ?? 0) <= 1 && !props.table,
+      helpText: "Pick model from a CMS Data Loader",
+      description:
+        "Usually not used! Only with multiple CMS Data Loaders, use this to choose which to show. Otherwise, go select the CMS Data Loader if you want to load different data.",
       options: (_: any, ctx: TableContextData | null) =>
         mkTableOptions(ctx?.tables),
       defaultValueHint: (_, ctx) => ctx?.table,
