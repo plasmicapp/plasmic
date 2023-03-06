7979893d27674fb9bdd89de62c5619d57ef2fbfd
Felipe Mota <fmota@plasmic.app>
2023-03-06 11:17:22 -0300

    [app-auth] Extend PlasmicPageGuard browser support
    Change-Id: I5e1e888b53622a0d2b7ac029f39a08e1fe837787

--- packages/react-web/src/auth/PlasmicPageGuard.tsx
+++ packages/react-web/src/auth/PlasmicPageGuard.tsx
@@ -1,6 +1,17 @@
 import { usePlasmicDataSourceContext } from "@plasmicapp/data-sources-context";
 import React from "react";
 
+// https://stackoverflow.com/a/2117523
+function uuidv4() {
+  // @ts-ignore
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
+  );
+}
+
 async function triggerLogin(appId: string, authorizeEndpoint: string) {
   async function sha256(text: string) {
     const encoder = new TextEncoder();
@@ -15,7 +26,7 @@ async function triggerLogin(appId: string, authorizeEndpoint: string) {
 
   const continueTo = window.location.href;
   const state = JSON.stringify({ continueTo });
-  const code_verifier = (crypto as any).randomUUID();
+  const code_verifier = uuidv4();
   localStorage.setItem("code_verifier", code_verifier);
   const code_challenge = await sha256(code_verifier);
 
@@ -39,14 +50,20 @@ interface PlasmicPageGuardProps {
 }
 
 export function PlasmicPageGuard(props: PlasmicPageGuardProps) {
-  const { appId, authorizeEndpoint, minRole, canTriggerLogin, children } =
-    props;
+  const {
+    appId,
+    authorizeEndpoint,
+    minRole,
+    canTriggerLogin,
+    children,
+  } = props;
 
   const dataSourceCtxValue = usePlasmicDataSourceContext();
 
   React.useEffect(() => {
     if (canTriggerLogin) {
       if (
+        minRole &&
         dataSourceCtxValue &&
         "isUserLoading" in dataSourceCtxValue &&
         !dataSourceCtxValue.isUserLoading &&
@@ -55,7 +72,7 @@ export function PlasmicPageGuard(props: PlasmicPageGuardProps) {
         triggerLogin(appId, authorizeEndpoint);
       }
     }
-  }, [dataSourceCtxValue, appId, authorizeEndpoint, canTriggerLogin]);
+  }, [dataSourceCtxValue, appId, authorizeEndpoint, canTriggerLogin, minRole]);
 
   function canUserViewPage() {
     if (!minRole) {
