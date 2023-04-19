e045f2214f18bd03062805c2b34dbc6f79b69a3f
Jason Long <j@jaslong.com>
2023-04-19 13:32:16 -0700

    [loader-nextjs] tsdx -> tsc
    Change-Id: I43dec869ee1e0b15a83ad6081be281704f399842

--- packages/loader-nextjs/react-server-conditional.d.ts
+++ packages/loader-nextjs/react-server-conditional.d.ts
@@ -1,4 +1,6 @@
-export * from './dist-react-server/index-react-server';
+import { initPlasmicLoader } from './dist/index';
+
 export * from './dist/index';
+export * from './dist/react-server';
 // Disambiguate initPlasmicLoader, prefer the "default" version since it is more permissive.
-export { initPlasmicLoader } from './dist/index';
+export { initPlasmicLoader };
