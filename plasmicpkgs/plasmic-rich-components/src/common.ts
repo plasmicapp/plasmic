28f09051daa8149068b6e8625c851cdf71651f63
Yang Zhang <yang@plasmic.app>
2023-05-21 17:47:45 -0700

    [theme] Add color scheme for RichLayout
    Change-Id: Ic19a6e7f44fe304081b4739ebc23bba8d41882c8

--- plasmicpkgs/plasmic-rich-components/src/common.ts
+++ plasmicpkgs/plasmic-rich-components/src/common.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { tinycolor } from "@ctrl/tinycolor";
 
 export function useIsClient() {
   const [loaded, setLoaded] = useState(false);
@@ -10,4 +11,9 @@ export function useIsClient() {
 
 export function capitalize(text: string) {
   return text.slice(0, 1).toUpperCase() + text.slice(1);
+}
+
+export function isLight(color: string) {
+  const { r, g, b } = tinycolor(color).toRgb();
+  return r * 0.299 + g * 0.587 + b * 0.114 > 186;
 }
