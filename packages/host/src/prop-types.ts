e8259c9cd46a6e37b137a7010b8809b605cdc955
Chung Wu <chungwu@plasmic.app>
2023-07-02 11:10:11 -0700

    [i18n] Can designate plain string props as localizable
    Change-Id: I5044b709c9cd7af63d112506079560379dee98cd

--- packages/host/src/prop-types.ts
+++ packages/host/src/prop-types.ts
@@ -121,6 +121,7 @@ export interface PropTypeBaseDefault<P, T>
 interface PlainStringType<P> extends PropTypeBaseDefault<P, string> {
   type: "string";
   control?: "default" | "large";
+  isLocalizable?: boolean;
 }
 
 interface CodeStringType<P> extends PropTypeBaseDefault<P, string> {
