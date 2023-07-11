6b938576ee7115b4496fd31347b2c5be6848a63e
Chung Wu <chungwu@plasmic.app>
2023-07-11 08:59:29 -0700

    [cli] Also build cli/lib.ts as output
    It is used by guidewire
    
    Change-Id: I74e877ce688ef6fc4a23888ee5cda1ebe90318b6

--- packages/cli/build.sh
+++ packages/cli/build.sh
@@ -3,7 +3,7 @@
 set -o errexit -o nounset
 
 ./node_modules/.bin/eslint 'src/**'
-./node_modules/.bin/esbuild src/index.ts --outfile=./dist/index.js --bundle --platform=node --format=cjs
+./node_modules/.bin/esbuild src/index.ts src/lib.ts --outdir=./dist --bundle --platform=node --format=cjs
 tsc --emitDeclarationOnly
 cp ./tsconfig-transform.json ./dist
 typescript-json-schema tsconfig.json 'PlasmicConfig' --excludePrivate --required > ./dist/plasmic.schema.json
