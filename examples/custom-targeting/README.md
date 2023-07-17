7394e83e1a1fdaf28c866b8fb7e49cabd8c14285
Felipe Mota <fmota@plasmic.app>
2023-07-17 20:55:01 -0300

    [custom-targetting] Example
    Change-Id: Ie230ee26a20c6e952e53a630a801c4a88846217b

---
+++ examples/custom-targeting/README.md
@@ -0,0 +1,38 @@
+# Custom Targeting Example
+
+This is an example of how to use Plasmic with custom targeting. Following the code in [Docs](https://docs.plasmic.app/learn/rendering-variations/).
+
+- In [plasmic-init](./plasmic-init.ts) it's defined the custom targetting traits `utm_source` and `browser`;
+- In [middleware](./middleware.ts) both the values of `utm_source` and `browser` are passed to `getMiddlewareResponse`;
+- In [Catch All](./pages/[[...catchall]].tsx) the `getActiveVariation` function is called to extract the variation based on the custom targetting traits.
+
+To open the corresponding Plasmic project (cloning it if you wish):
+
+https://studio.plasmic.app/projects/qSU617xDVJeD8V18Bsr4AA
+
+A live demo is available at:
+
+https://custom-target-plasmic.vercel.app/
+
+The targetting shows different variations based on the following rules:
+- `browser` = `chrome`
+- `utm_source` = `google`
+
+By going to the following URL:
+
+https://custom-target-plasmic.vercel.app/?utm_source=google
+
+The variation with utm_source = google will be shown.
+By using different browsers, the variation with browser = chrome will be shown.
+
+## Learn More
+
+With Plasmic, you can enable non-developers on your team to publish pages and content into your website or app.
+
+To learn more about Plasmic, take a look at the following resources:
+
+- [Plasmic Website](https://www.plasmic.app/)
+- [Plasmic Documentation](https://docs.plasmic.app/learn/)
+- [Plasmic Slack Community](https://www.plasmic.app/slack)
+
+You can check out [the Plasmic GitHub repository](https://github.com/plasmicapp/plasmic) - your feedback and contributions are welcome!
---
+++ examples/custom-targeting/middleware.ts
@@ -0,0 +1,56 @@
+import { NextRequest, NextResponse, userAgent } from "next/server";
+import { getMiddlewareResponse } from "@plasmicapp/loader-nextjs/edge";
+
+// Exclude paths that are definitely not Plasmic pages with variations
+export const config = {
+  matcher: ["/:path((?!_next/|api/|favicon\\.ico|plasmic-host).*)"],
+};
+
+export async function middleware(req: NextRequest) {
+  // Only pick a dynamic variation for GET requests
+  if (req.method !== "GET") {
+    return;
+  }
+
+  // NextJs uses https://github.com/faisalman/ua-parser-js to parse
+  const ua = userAgent(req);
+  const browser = ua.browser.name?.includes("Chrome")
+    ? "Chrome"
+    : ua.browser.name?.includes("Safari")
+    ? "Safari"
+    : "Other";
+
+  const newUrl = req.nextUrl.clone();
+
+  const PLASMIC_SEED = req.cookies.get("plasmic_seed");
+
+  // Rewrite to a new pathname that encodes the custom traits for
+  // this request, as well as some randomness for A/B tests
+  const { pathname, cookies } = getMiddlewareResponse({
+    path: newUrl.pathname,
+    traits: {
+      // Add values for custom traits that you are using; these are
+      // likely read off of the cookie.
+      utm_source: req.nextUrl.searchParams.get("utm_source") ?? "",
+      browser,
+    },
+    cookies: {
+      ...(PLASMIC_SEED ? { plasmic_seed: PLASMIC_SEED.value } : {}),
+    },
+  });
+
+  // Rewrite the response to use this new pathname
+  newUrl.pathname = pathname;
+  const res = NextResponse.rewrite(newUrl);
+
+  // Save anything that needs to be saved in a cookie -- specifically,
+  // the custom trait that corresponds to the random seed. The same
+  // random seed will be used to pick the A/B test bucket each time
+  // the user visits, to ensure that a visitor will always see the
+  // same A/B test bucket.
+  cookies.forEach((cookie) => {
+    res.cookies.set(cookie.key, cookie.value);
+  });
+
+  return res;
+}
