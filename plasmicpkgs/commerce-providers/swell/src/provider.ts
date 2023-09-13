a6227291a6348c65ddb9deb8905c15f801287daf
Samuel <samuel@plasmic.app>
2023-09-13 15:18:24 -0300

    fix type error in swell package
    Change-Id: I25cc510069beb10072e3e9c069517ba0fdf168e2

--- plasmicpkgs/commerce-providers/swell/src/provider.ts
+++ plasmicpkgs/commerce-providers/swell/src/provider.ts
@@ -3,37 +3,37 @@
   Changes: Added storeId and publicKey parameters
 */
 // @ts-ignore
-import swell from 'swell-js'
+import swell, { init } from "swell-js";
 
-import { CommerceExtraFeatures, Provider } from '@plasmicpkgs/commerce'
-import { SWELL_CHECKOUT_ID_COOKIE } from './const'
-import { handler as useCart } from './cart/use-cart'
-import { handler as useAddItem } from './cart/use-add-item'
-import { handler as useUpdateItem } from './cart/use-update-item'
-import { handler as useRemoveItem } from './cart/use-remove-item'
-import { handler as useSearch } from './product/use-search'
-import { handler as useProduct } from './product/use-product'
-import { handler as useCategories } from "./site/use-categories"
-import { handler as useBrands } from "./site/use-brands"
-import fetcher from './fetcher'
-import { Fetcher } from '@plasmicpkgs/commerce'
+import { CommerceExtraFeatures, Provider } from "@plasmicpkgs/commerce";
+import { SWELL_CHECKOUT_ID_COOKIE } from "./const";
+import { handler as useCart } from "./cart/use-cart";
+import { handler as useAddItem } from "./cart/use-add-item";
+import { handler as useUpdateItem } from "./cart/use-update-item";
+import { handler as useRemoveItem } from "./cart/use-remove-item";
+import { handler as useSearch } from "./product/use-search";
+import { handler as useProduct } from "./product/use-product";
+import { handler as useCategories } from "./site/use-categories";
+import { handler as useBrands } from "./site/use-brands";
+import fetcher from "./fetcher";
+import { Fetcher } from "@plasmicpkgs/commerce";
 
 export const getSwellProvider = (storeId: string, publicKey: string) => {
-  swell.init(storeId, publicKey);
+  init(storeId, publicKey);
 
   return {
-    locale: 'en-us',
+    locale: "en-us",
     cartCookie: SWELL_CHECKOUT_ID_COOKIE,
     swell,
     fetcher,
     cart: { useCart, useAddItem, useUpdateItem, useRemoveItem },
     products: { useSearch, useProduct },
     site: { useCategories, useBrands },
-    extraFeatures: { 
-      includeSubCategories: true
-    }
-  }
-}
+    extraFeatures: {
+      includeSubCategories: true,
+    },
+  };
+};
 
 export type SwellProvider = {
   locale: string;
@@ -43,17 +43,17 @@ export type SwellProvider = {
     useCart: typeof useCart;
     useAddItem: typeof useAddItem;
     useUpdateItem: typeof useUpdateItem;
-    useRemoveItem: typeof useRemoveItem
+    useRemoveItem: typeof useRemoveItem;
   };
   products: {
-    useSearch: typeof useSearch
+    useSearch: typeof useSearch;
   };
   site: {
-    useCategories: typeof useCategories
-    useBrands: typeof useBrands
+    useCategories: typeof useCategories;
+    useBrands: typeof useBrands;
   };
-  extraFeatures: CommerceExtraFeatures
-  swell: any
-}
+  extraFeatures: CommerceExtraFeatures;
+  swell: any;
+};
 
-export default swell;
+export default swell as any;
