48a804c567ee36a9cc153a15e3ca6a3215a2a719
Tiago Madeira <tiago@plasmic.app>
2023-10-18 17:04:58 +0000

    Merge "Fix upgrade-internal script"
    GitOrigin-RevId: 3609628af909213e41a776dfafe52deb3fbbf6ca

--- scripts/upgrade-internal.bash
+++ scripts/upgrade-internal.bash
@@ -10,11 +10,18 @@ npx --yes concurrently \
   --kill-others-on-fail \
   --max-processes 1 \
   --names canvas-packages,host-test,loader-bundle-env,loader-html-hydrate,react-web-bundle,sub,wab \
-  'cd canvas-packages && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd host-test && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd loader-bundle-env && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd react-web-bundle && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd sub && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
-  'cd wab && yarn --mutex network upgrade --latest --scope @plasmicapp --scope @plasmicpkgs' \
+  'cd canvas-packages && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd canvas-packages && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd host-test && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd host-test && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd loader-bundle-env && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd loader-bundle-env && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd react-web-bundle && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd react-web-bundle && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd sub && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd sub && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd wab && yarn --mutex network upgrade --latest --scope @plasmicapp' \
+  'cd wab && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
   # end
