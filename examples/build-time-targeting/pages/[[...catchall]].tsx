5b388c713be0951ccb5ce0e4ee201c413b24a574
Felipe Mota <fmota@plasmic.app>
2023-10-24 23:58:05 +0000

    Merge "[splits] Add example of custom targeting with fallback false"
    GitOrigin-RevId: 6abeafd09765d470f9d2c77675ac03c15b5e68f8

---
+++ examples/build-time-targeting/pages/[[...catchall]].tsx
@@ -0,0 +1,106 @@
+import {
+  ComponentRenderData,
+  extractPlasmicQueryData,
+  PlasmicComponent,
+  PlasmicRootProvider,
+} from "@plasmicapp/loader-nextjs";
+import type { GetStaticPaths, GetStaticProps } from "next";
+
+import { PLASMIC } from "@/plasmic-init";
+import {
+  generateAllPathsWithTraits,
+  getActiveVariation,
+  rewriteWithoutTraits,
+} from "@plasmicapp/loader-nextjs/edge";
+import Error from "next/error";
+import { useRouter } from "next/router";
+
+export default function PlasmicLoaderPage(props: {
+  plasmicData?: ComponentRenderData;
+  queryCache?: Record<string, any>;
+  variation?: Record<string, string>;
+}) {
+  const { plasmicData, queryCache, variation } = props;
+  const router = useRouter();
+  if (!plasmicData || plasmicData.entryCompMetas.length === 0) {
+    return <Error statusCode={404} />;
+  }
+  const pageMeta = plasmicData.entryCompMetas[0];
+  return (
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      prefetchedQueryData={queryCache}
+      pageParams={pageMeta.params}
+      pageQuery={router.query}
+      variation={variation}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const { catchall } = context.params ?? {};
+  const rawPlasmicPath =
+    typeof catchall === "string"
+      ? catchall
+      : Array.isArray(catchall)
+      ? `/${catchall.join("/")}`
+      : "/";
+
+  // Parse the path and extract the traits
+  const { path: plasmicPath, traits } = rewriteWithoutTraits(rawPlasmicPath);
+
+  const plasmicData = await PLASMIC.maybeFetchComponentData(plasmicPath);
+  if (!plasmicData) {
+    // non-Plasmic catch-all
+    return { props: {} };
+  }
+
+  // Get the active variation for this page
+  const variation = getActiveVariation({
+    splits: PLASMIC.getActiveSplits(),
+    traits,
+    path: plasmicPath,
+  });
+
+  const pageMeta = plasmicData.entryCompMetas[0];
+  // Cache the necessary data fetched for the page
+  const queryCache = await extractPlasmicQueryData(
+    <PlasmicRootProvider
+      loader={PLASMIC}
+      prefetchedData={plasmicData}
+      pageParams={pageMeta.params}
+      variation={variation}
+    >
+      <PlasmicComponent component={pageMeta.displayName} />
+    </PlasmicRootProvider>
+  );
+  // Use revalidate if you want incremental static regeneration
+  return { props: { plasmicData, queryCache, variation }, revalidate: 60 };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const pageModules = await PLASMIC.fetchPages();
+  function* gen() {
+    for (const page of pageModules) {
+      // Generate all possible paths for this page including all variations
+      const allPaths = generateAllPathsWithTraits(page.path, {
+        browser: ["Chrome", "Safari", "Other"],
+        utm_source: ["google", "facebook"],
+      });
+      for (const path of allPaths) {
+        yield {
+          params: {
+            catchall: path.substring(1).split("/"),
+          },
+        };
+      }
+    }
+  }
+  return {
+    paths: Array.from(gen()),
+    fallback: false,
+  };
+};
