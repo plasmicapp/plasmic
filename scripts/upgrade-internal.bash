418303fb75c24a0eea540b6e23d6d3a009f31fe7
Yang Zhang <yang@plasmic.app>
2023-10-26 18:22:09 +0000

    Merge "[ci] Fix local upgrade-internal"
    GitOrigin-RevId: 6d6b348d745744c75c945fcf55caca8aa582a20e

--- scripts/upgrade-internal.bash
+++ scripts/upgrade-internal.bash
@@ -10,18 +10,11 @@ npx --yes concurrently \
   --kill-others-on-fail \
   --max-processes 1 \
   --names canvas-packages,host-test,loader-bundle-env,loader-html-hydrate,react-web-bundle,sub,wab \
-  'cd canvas-packages && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd canvas-packages && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd host-test && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd host-test && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd loader-bundle-env && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd loader-bundle-env && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd react-web-bundle && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd react-web-bundle && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd sub && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd sub && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
-  'cd wab && yarn --mutex network upgrade --latest --scope @plasmicapp' \
-  'cd wab && yarn --mutex network upgrade --latest --scope @plasmicpkgs' \
+  'cd canvas-packages && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd host-test && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd loader-bundle-env && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd react-web-bundle && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd sub && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd wab && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
   # end
