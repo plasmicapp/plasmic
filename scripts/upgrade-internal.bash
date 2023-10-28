684df0e1f229a7d5816109df8a224db551869ea7
Yang Zhang <yang@plasmic.app>
2023-10-28 00:14:26 +0000

    Merge changes from topic "publicci"
    * changes:
      [ci] Build public version
      [ci] Fix retries
    
    GitOrigin-RevId: 9d6c70cf6f393d884d7bf10a3169e99913ea21bf

--- scripts/upgrade-internal.bash
+++ scripts/upgrade-internal.bash
@@ -6,15 +6,17 @@ cd platform
 
 # For now, this doesn't do achieve much other than labeling output, since yarn needs mutex so no parallelism.
 
+# flock: For some reason, yarn --mutex contention can cause yarn install to just quietly abort!
 npx --yes concurrently \
   --kill-others-on-fail \
   --max-processes 1 \
+  --timings \
   --names canvas-packages,host-test,loader-bundle-env,loader-html-hydrate,react-web-bundle,sub,wab \
-  'cd canvas-packages && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd host-test && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd loader-bundle-env && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd loader-html-hydrate && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd react-web-bundle && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd sub && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
-  'cd wab && yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd canvas-packages && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd host-test && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd loader-bundle-env && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd loader-html-hydrate && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd react-web-bundle && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd sub && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
+  'cd wab && flock /tmp/yarn-mutex yarn --mutex network upgrade --latest --pattern "@plasmic(app|pkgs)/*"' \
   # end
