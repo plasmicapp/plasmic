a65546205c1e26784ffcc4ba5b0d83b056f9f4c1
Sarah Ahmed <sarah@plasmic.app>
2023-11-02 03:48:26 +0000

    Merge "[hostless] timer tweaks"
    GitOrigin-RevId: 11bfa71020b0d0d61815a12c2518d481f4c65df4

--- plasmicpkgs/plasmic-basic-components/src/Timer.tsx
+++ plasmicpkgs/plasmic-basic-components/src/Timer.tsx
@@ -1,13 +1,15 @@
 import registerComponent, {
   CodeComponentMeta,
 } from "@plasmicapp/host/registerComponent";
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useIsomorphicLayoutEffect } from "./common";
 
 export interface TimerProps {
   onTick: () => void;
   isRunning?: boolean;
   intervalSeconds?: number;
+  runWhileEditing: boolean;
+  "data-plasmic-canvas-envs"?: number;
 }
 
 export function useInterval(callback: () => void, delay: number | null) {
@@ -36,11 +38,16 @@ export default function Timer({
   intervalSeconds = 1,
   isRunning = false,
   onTick = () => {},
+  runWhileEditing,
+  "data-plasmic-canvas-envs": canvasId,
 }: TimerProps) {
+  const isEditMode = useMemo(() => canvasId !== undefined, [canvasId]);
   useInterval(
     onTick,
     // Delay in milliseconds or null to stop it
-    isRunning ? intervalSeconds * 1000 : null
+    isRunning && (isEditMode ? runWhileEditing : true)
+      ? intervalSeconds * 1000
+      : null
   );
   return null;
 }
@@ -54,17 +61,25 @@ export const timerMeta: CodeComponentMeta<TimerProps> = {
   props: {
     onTick: {
       type: "eventHandler",
-      displayName: "Run periodically",
+      displayName: "Run this every interval",
       description: "Actions to run periodically",
       argTypes: [],
     },
     isRunning: {
       type: "boolean",
       displayName: "Is Running?",
+      defaultValue: true,
+    },
+    runWhileEditing: {
+      type: "boolean",
+      displayName: "Run while editing",
+      description:
+        "Normally this only runs in the live site or in preview mode, but you can force it to run even while you are editing in the canvas (Please enable interactive mode to observe state changes)",
+      defaultValue: false,
     },
     intervalSeconds: {
       type: "number",
-      displayName: "Seconds",
+      displayName: "Interval (seconds)",
       description: "Interval in seconds",
     },
   },
