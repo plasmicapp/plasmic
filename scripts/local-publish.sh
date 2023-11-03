2fd32fdea76e4164b2b3bc7d008f251067d52192
Yang Zhang <yang@plasmic.app>
2023-11-03 11:33:30 -0700

    Fix lerna/nx issues
    lerna not compat with nx 17+, lerna exec hangs:
    
    https://github.com/lerna/lerna/issues/3875
    
    local-publish unpublish was actually not doing anything if called directly, so later publishes would fail.
    
    local-publish unpublish can fail-fast, causing later publish attempts to fail.
    
    yarn.lock was out of date.
    
    .nx dir should be gitignored.
    
    Change-Id: I9acb32b050a87943d1985d801c2ab2703ffc6b36
    GitOrigin-RevId: 392ff9f5ecd5c8fed89e39ccaae8e5f94a5de851

--- scripts/local-publish.sh
+++ scripts/local-publish.sh
@@ -5,7 +5,7 @@ pkg=$1
 echo "Locally publishing $pkg"
 
 # First, locally unpublish
-./node_modules/.bin/lerna exec --loglevel=silent --scope "$pkg" --include-dependencies -- npm --registry=http://localhost:4873 unpublish -f "${LERNA_PACKAGE_NAME}"
+./node_modules/.bin/lerna exec --loglevel=silent --scope "$pkg" --include-dependencies --no-bail -- npm --registry=http://localhost:4873 unpublish -f "\${LERNA_PACKAGE_NAME}"
 
 # Next, build the packages using nx
 ./node_modules/.bin/lerna exec --loglevel=silent --scope "$pkg" --include-dependencies -- yarn nx build
