ca21c914f3ac73a5aea781b0a3a30c93140ff877
Ícaro Guerra <icaro@plasmic.app>
2023-12-05 18:50:47 +0000

    Merge "[fix] remove pybackend references and add folder to OS"
    GitOrigin-RevId: 57ccf024d404a7f46f6595073f87be28ab507f16

--- platform/wab/tools/start.bash
+++ platform/wab/tools/start.bash
@@ -19,7 +19,6 @@ make
 do-screen wab-dev-server 'TRANSPILER=swc nice -n +30 yarn start'
 do-screen wab-watch 'yarn host-server & nice -n +30 yarn watch-css & cd ../sub/; yarn; nice -n +30 yarn watch & cd ../canvas-packages/; yarn; nice -n +30 yarn watch & cd ../react-web-bundle/; yarn; nice -n +30 yarn watch & cd ../live-frame/; yarn; nice -n +30 yarn watch & cd ../loader-html-hydrate/; yarn; nice -n +30 yarn build &'
 do-screen wab-node-server 'nice -n +30 yarn backend'
-do-screen py-backend 'cd ../pybackend/ && bash ./start.sh'
 
 { sleep 1; screen -S wab-screens -X source tools/layout.screen; } &
 screen -dR wab-screens
