e4c02f9f03c8b80bf389366acc1ffcd3784a19f2
Tiago Madeira <tiago@plasmic.app>
2023-12-13 15:50:58 +0000

    Merge "[gha] Annotate react-web jest results"
    GitOrigin-RevId: f2d54fd285572a7b6e51e206ebae7c0a8249c127

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('jest').Config} */
 module.exports = {
+  reporters: [
+    process.env.CI ? ["github-actions", { silent: false }] : "default",
+    "summary",
+  ],
   testRegex:
     ".*\\/(packages|plasmicpkgs)\\/.*\\.(spec|test)\\.(js|jsx|ts|tsx)$",
   // TODO Should really be running jest from each package rather than the root.
