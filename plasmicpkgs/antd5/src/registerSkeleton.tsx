9ca431b390a25015b6c99ea88bc9b5e1fff6f485
Georg Malahov <georg@malahov.io>
2023-12-19 18:03:56 +0100

    fix: extend properties configuration

--- plasmicpkgs/antd5/src/registerSkeleton.tsx
+++ plasmicpkgs/antd5/src/registerSkeleton.tsx
@@ -3,33 +3,100 @@ import React from "react";
 import { Registerable, registerComponentHelper } from "./utils";
 
 export type AntdSkeletonProps = SkeletonProps & {
-  type?: "Basic" | "Button" | "Avatar" | "Input" | "Image" | "Node";
-};
+  type?: 'Basic' | 'Button' | 'Avatar' | 'Input' | 'Image' | 'Node'
+  width?: string | number | (string | number)[]
+  widthTitle?: string
+  rows?: number
+  shape?: 'circle' | 'square'
+  size?: 'large' | 'small' | 'default'
+  customClassName?: string
+}
 
 export function AntdSkeleton(props: AntdSkeletonProps) {
-  const { type, loading, children } = props;
-  
-  if (type === "Button") {
-    return loading ? <Skeleton.Button {...props} /> : children;
+  const {
+    type,
+    paragraph,
+    avatar,
+    title,
+    shape,
+    size,
+    loading,
+    width,
+    widthTitle,
+    rows,
+    className: rootClassName,
+    customClassName,
+    children,
+  } = props
+
+  if (type === 'Button') {
+    return loading ? (
+      <Skeleton.Button
+        {...props}
+        rootClassName={rootClassName}
+        className={customClassName}
+      />
+    ) : (
+      children
+    )
   }
 
-  if (type === "Avatar") {
-    return loading ? <Skeleton.Avatar {...props} />: children;
+  if (type === 'Avatar') {
+    return loading ? (
+      <Skeleton.Avatar
+        {...props}
+        rootClassName={rootClassName}
+        className={customClassName}
+      />
+    ) : (
+      children
+    )
   }
 
-  if (type === "Input") {
-    return loading ? <Skeleton.Input {...props} /> : children;
+  if (type === 'Input') {
+    return loading ? (
+      <Skeleton.Input
+        {...props}
+        rootClassName={rootClassName}
+        className={customClassName}
+      />
+    ) : (
+      children
+    )
   }
 
-  if (type === "Image") {
-    return loading ? <Skeleton.Image {...props} /> : children;
+  if (type === 'Image') {
+    return loading ? (
+      <Skeleton.Image
+        {...props}
+        rootClassName={rootClassName}
+        className={customClassName}
+      />
+    ) : (
+      children
+    )
   }
 
-  if (type === "Node") {
-    return <Skeleton.Node {...props} />;
+  if (type === 'Node') {
+    return (
+      <Skeleton.Node
+        {...props}
+        rootClassName={props.className}
+        className={customClassName}
+      />
+    )
   }
 
-  return <Skeleton {...props} />;
+  return (
+    <Skeleton
+      {...props}
+      paragraph={paragraph ? { rows, width } : false}
+      avatar={avatar ? { shape, size } : false}
+      title={title ? { width: widthTitle, className: customClassName } : false}
+      rootClassName={rootClassName}
+      className={customClassName}
+    />
+  )
 }
 
 export const skeletonComponentName = "plasmic-antd5-skeleton";
@@ -37,101 +104,110 @@ export const skeletonComponentName = "plasmic-antd5-skeleton";
 export function registerSkeleton(loader?: Registerable) {
   registerComponentHelper(loader, AntdSkeleton, {
     name: skeletonComponentName,
-    displayName: "Skeleton",
+    displayName: 'Skeleton',
     props: {
-      children: "slot",
+      children: 'slot',
       type: {
-        type: "choice",
-        defaultValueHint: "Basic",
-        options: ["Basic", "Button","Avatar", "Input", "Image", "Node"],
+        type: 'choice',
+        defaultValue: 'Basic',
+        options: ['Basic', 'Button', 'Avatar', 'Input', 'Image', 'Node'],
       },
       active: {
-        type: "boolean",
-        description: "Show animation effect",
-        hidden: (ps) => ps.type !== "Basic" && ps.type !== "Button" && ps.type !== "Avatar" && ps.type !== "Input",
-        defaultValueHint: false,
+        type: 'boolean',
+        description: 'Show animation effect',
+        defaultValue: false,
       },
       avatar: {
-        type: "boolean",
-        description: "Show avatar placeholder",
-        hidden: (ps) => ps.type !== "Basic",
-        defaultValueHint: false,
+        type: 'boolean',
+        description: 'Show avatar placeholder',
+        hidden: (ps) => ps.type !== 'Basic',
+        defaultValue: false,
       },
       loading: {
-        type: "boolean",
-        description: "Display the skeleton when true",
-        defaultValueHint: false,
+        type: 'boolean',
+        description: 'Display the skeleton when true',
+        defaultValue: false,
       },
       paragraph: {
-        type: "boolean",
-        description: "Show paragraph placeholder",
-        hidden: (ps) => ps.type !== "Basic",
-        defaultValueHint: true,
+        type: 'boolean',
+        description: 'Show paragraph placeholder',
+        hidden: (ps) => ps.type !== 'Basic',
+        defaultValue: true,
       },
       round: {
-        type: "boolean",
-        description: "Show paragraph and title radius when true",
-        hidden: (ps) => ps.type !== "Basic",
-        defaultValueHint: false,
+        type: 'boolean',
+        description: 'Show paragraph and title radius when true',
+        hidden: (ps) => ps.type !== 'Basic',
+        defaultValue: false,
       },
       title: {
-        type: "boolean",
-        description: "Show title placeholder",
-        hidden: (ps) => ps.type !== "Basic",
-        defaultValueHint: true,
+        type: 'boolean',
+        description: 'Show title placeholder',
+        hidden: (ps) => ps.type !== 'Basic',
+        defaultValue: true,
       },
       size: {
-        type: "choice",
-        defaultValueHint: "default",
+        type: 'choice',
+        defaultValue: 'default',
         description: `Size of the skeleton for types: Avatar, Button and Input`,
-        hidden: (ps) => ps.type !== "Avatar" && ps.type !== "Button" && ps.type !== "Input" && ps.avatar !== true,
+        hidden: (ps) =>
+          ps.type !== 'Avatar' &&
+          ps.type !== 'Button' &&
+          ps.type !== 'Input' &&
+          ps.avatar !== true,
         advanced: true,
-        options: ["large", "small", "default"],
+        options: ['large', 'small', 'default'],
       },
       // SkeletonAvatarProps
       shape: {
-        type: "choice",
-        defaultValueHint: "circle",
+        type: 'choice',
+        defaultValue: 'circle',
         description: `Set the shape of avatar`,
-        hidden: (ps) => ps.type !== "Avatar" && ps.avatar !== true,
+        hidden: (ps) => ps.type !== 'Avatar' && ps.avatar !== true,
         advanced: true,
-        options: ["circle", "square"],
+        options: ['circle', 'square'],
       },
       // SkeletonTitleProps
       widthTitle: {
-        type: "string",
-        description: "Width of the title",
+        type: 'string',
+        description: 'Width of the title',
         hidden: (ps) => !ps.title,
         advanced: true,
       },
       // SkeletonParagraphProps
       width: {
-        type: "array",
-        description: "Set the width of paragraph. When width is an Array, it can set the width of each row. Otherwise only set the last row width",
-        hidden: (ps) => !ps.paragraph && ps.type !== "Basic",
+        type: 'array',
+        description:
+          'Set the width of paragraph. When width is an Array, it can set the width of each row. Otherwise only set the last row width',
+        hidden: (ps) => !ps.paragraph && ps.type !== 'Basic',
         advanced: true,
       },
       rows: {
-        type: "number",
-        description: "Set the row count of paragraph",
-        hidden: (ps) => !ps.paragraph && ps.type !== "Basic",
+        type: 'number',
+        description: 'Set the row count of paragraph',
+        hidden: (ps) => !ps.paragraph && ps.type !== 'Basic',
         advanced: true,
       },
       // SkeletonButtonProps
       shapeButton: {
-        type: "choice",
-        defaultValueHint: "default",
+        type: 'choice',
+        defaultValue: 'default',
         description: `Set the shape of button`,
-        hidden: (ps) => ps.type !== "Button",
+        hidden: (ps) => ps.type !== 'Button',
         advanced: true,
-        options: ["default", "circle", "round", "square"],
+        options: ['default', 'circle', 'round', 'square'],
       },
       block: {
-        type: "boolean",
-        description: "Option to fit button width to its parent width",
-        hidden: (ps) => ps.type !== "Button",
-        defaultValueHint: false,
+        type: 'boolean',
+        description: 'Option to fit button width to its parent width',
+        hidden: (ps) => ps.type !== 'Button' && ps.type !== 'Input',
+        defaultValue: false,
         advanced: true,
+      },
+      customClassName: {
+        type: 'string',
+        displayName: 'Class Name',
+        description: 'Custom class name for Node skeleton for custom styling',
       },
     },
     importPath: "@plasmicpkgs/antd5/skinny/registerSkeleton",
