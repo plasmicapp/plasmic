5448c480186e3baa72d4c59021a5896ff432b222
Felipe Mota <fmota@plasmic.app>
2024-01-08 19:26:21 -0300

    [host] Update required version of @plasmicapp/host
    Change-Id: I119e74d50b4d257955c02b321a46f851b35c4c1e
    GitOrigin-RevId: ef2eb39cec8c70c1abe5b5a3abc54223a0abc36c

--- platform/wab/src/wab/shared/required-versions.ts
+++ platform/wab/src/wab/shared/required-versions.ts
@@ -1,7 +1,7 @@
 export const requiredPackageVersions = {
   "@plasmicapp/loader": "0.0.1",
   "@plasmicapp/cli": "0.1.324",
-  "@plasmicapp/host": "1.0.145",
+  "@plasmicapp/host": "1.0.188",
   "@plasmicapp/react-web": "0.2.266",
   "@plasmicapp/react-web-runtime": "0.0.1",
 };
