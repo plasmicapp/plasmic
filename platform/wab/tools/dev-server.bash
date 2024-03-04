2be1dd4ff96d3894d87fab8c1b672633bdedfbc9
Tiago Madeira <tiago@plasmic.app>
2024-03-04 13:24:00 -0300

    Remove WAB_HTTPS from dev-server (it does not work)
    Change-Id: Ia84b2155a0cfd7da7b22bd5db4528ebed9c6ab2f
    GitOrigin-RevId: 4902089dafc40206fdd2e7e36c7bfb8cf05ad097

--- platform/wab/tools/dev-server.bash
+++ platform/wab/tools/dev-server.bash
@@ -1,16 +1,5 @@
 #!/usr/bin/env bash
 
-# This doesn't work, need to upgrade react-scripts to a version that supports SSL_CRT_FILE etc.
-# Instead, for now, use a third-party tool like tools/https.bash or stunnel.
-if [[ $WAB_HTTPS ]]; then
-  if [[ ! -f localhost.pem || ! -f localhost-key.pem ]]; then
-    mkcert localhost
-  fi
-  export HTTPS=true
-  export SSL_CRT_FILE=localhost.pem
-  export SSL_KEY_FILE=localhost-key.pem
-fi
-
 cmd=${1:-start}
 port=${PORT:-3003}
 
