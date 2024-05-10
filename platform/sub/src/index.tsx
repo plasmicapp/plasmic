aeed2c827869960d489295a8e7fc232bc5b40766
Ícaro Julião <icaro@plasmic.app>
2024-05-10 16:19:43 -0300

    [on-prem] remove unused code from sub
    Change-Id: Ic71aacc1b4704372aa021f1607809b480de4bdef
    GitOrigin-RevId: a657daac0b0d590a8bf4fb725c5bd2223a86520c

--- platform/sub/src/index.tsx
+++ platform/sub/src/index.tsx
@@ -1,38 +1,11 @@
 // organize-imports-ignore
-import {
-  PlasmicCanvasHost,
-  unstable_registerFetcher as registerFetcher,
-} from "@plasmicapp/host";
+import { PlasmicCanvasHost } from "@plasmicapp/host";
 
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 
-export async function getBlogPosts(publishedOnly: true) {
-  console.log("BLOG POST WAIT");
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  console.log("BLOG POST DONE");
-  return {
-    blogPosts: [
-      { id: 42, title: "Baz", body: "Qux", isPublished: false },
-      { id: 16, title: "Foo", body: "Bar", isPublished: true },
-    ].filter((x) => !publishedOnly || x.isPublished),
-  };
-}
-
-registerFetcher(getBlogPosts, {
-  name: "getBlogPosts",
-  displayName: "get blog posts",
-  args: [
-    {
-      name: "publishedOnly",
-      type: "boolean",
-    },
-  ],
-  returns: "object",
-  importPath: "./src/components/Badge.tsx",
-});
-
 export function renderHostScaffold() {
+  debugger;
   const appRoot = document.querySelector(".app-root");
   if (appRoot) {
     const root = createRoot(appRoot);
