f00011454a45477e5c445d35eb5ebc461e150f5e
Jason Long <j@jaslong.com>
2024-06-19 01:43:07 -0700

    [wab] Delete "Hello, Uri" console.log
    Change-Id: I5b3047e1a0eb73d0edd02cabbb0c8f22d5cbd3a7
    GitOrigin-RevId: 2700acf38f61ad55f136306dda446a896f8bce86

--- platform/wab/src/wab/main.tsx
+++ platform/wab/src/wab/main.tsx
@@ -1,12 +1,11 @@
 import { main } from "@/wab/client/components/Shell";
-import * as mobx from "mobx";
-// import { main } from "./client/sandboxes/GraphqlSandbox";
 import { dbg } from "@/wab/dbg";
 import "@/wab/styles/antd-overrides.less";
 import "@/wab/styles/loader.scss";
 import "@/wab/styles/main.sass";
 import "jquery";
 import "jquery-serializejson";
+import * as mobx from "mobx";
 
 mobx.configure({
   enforceActions: "never",
@@ -18,10 +17,3 @@ dbg.mobx = mobx;
 // Main application code
 //
 main();
-
-// TODO Not sure if we always need to export something for build system to be
-// happy.
-export function greet(who: /*TWZ*/ string) {
-  return `Hello, ${who}`;
-}
-console.log(greet("Uri"));
