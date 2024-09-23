a2b6f69dcecc1df8ab423d76ea5f7db6a79cc3f6
Ícaro Julião <icaro@plasmic.app>
2024-09-23 18:06:47 -0300

    fix(radix-ui): remove hydration warning from Dialog for not using ref prop
    Change-Id: If58477ba65d4c271aafb75d1687a644f215e5150
    GitOrigin-RevId: f6ef35e9807e06a29beec79fd3bef469bd0461c4

--- plasmicpkgs/radix-ui/src/dialog.tsx
+++ plasmicpkgs/radix-ui/src/dialog.tsx
@@ -11,10 +11,10 @@ import { Registerable, registerComponentHelper } from "./reg-util";
 import {
   Animated,
   AnimatedProps,
-  animPropTypes,
   BaseStyles,
   EnterAnim,
   ExitAnim,
+  animPropTypes,
   overlayProps,
   overlayStates,
   prefixClasses,
@@ -25,7 +25,7 @@ import {
 export const DialogClose = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Close>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Close>
->((props) => (
+>((props, _ref) => (
   <DialogPrimitive.Close {...props} asChild>
     <div className={props.className}>
       {props.children ?? <X className={prefixClasses("h-4 w-4")} />}
