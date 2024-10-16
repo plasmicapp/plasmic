93f8b984aec13903ef56dc73c79e8455eb481163
Ícaro Guerra <icaro@plasmic.app>
2024-10-16 13:51:16 -0300

    fix: fix interactive variants
    GitOrigin-RevId: 616a7b12648db2826e80a607ee20e5335b19525e

---
+++ platform/wab/src/wab/server/bundle-migrations/235-variant-selectors-no-display-name-corrected.ts
@@ -0,0 +1,24 @@
+import { BundleMigrationType } from "@/wab/server/db/bundle-migration-utils";
+import { BundledMigrationFn } from "@/wab/server/db/BundleMigrator";
+import { pseudoSelectors } from "@/wab/shared/core/styles";
+
+const presetDisplayNamesToCssSelectors = new Map(
+  pseudoSelectors.map((opt) => [opt.displayName, opt.cssSelector])
+);
+
+export const migrate: BundledMigrationFn = async (bundle) => {
+  for (const inst of Object.values(bundle.map)) {
+    if (!(inst["__type"] === "Variant" && inst["selectors"])) {
+      continue;
+    }
+
+    const newSelectors = inst["selectors"].map(
+      (maybeDisplayName) =>
+        presetDisplayNamesToCssSelectors.get(maybeDisplayName) ??
+        maybeDisplayName
+    );
+    inst["selectors"] = newSelectors;
+  }
+};
+
+export const MIGRATION_TYPE: BundleMigrationType = "bundled";
