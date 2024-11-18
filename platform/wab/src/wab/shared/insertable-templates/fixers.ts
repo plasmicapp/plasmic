9c9cd440c3931e758faa1bfedf68e5637441f334
sarah <sarah@plasmic.app>
2024-11-18 15:24:21 +0500

    fix(wab): tokens are doubled when installing Plexus
    Change-Id: I7af246f1800e18c4f577b15b893c233c9bb8bdab
    GitOrigin-RevId: 02fcbb9dfd188378dc5d8c3d8b6bf75329b50ac1

--- platform/wab/src/wab/shared/insertable-templates/fixers.ts
+++ platform/wab/src/wab/shared/insertable-templates/fixers.ts
@@ -266,6 +266,9 @@ export function mkInsertableTokenImporter(
     if (oldToNewToken.has(oldToken)) {
       return oldToNewToken.get(oldToken)!;
     }
+    const namespacedTokenName = groupName
+      ? `${groupName}/${oldToken.name}`
+      : oldToken.name;
 
     // `targetTokens` won't consider tokens that have been added by `getOrAddToken`
     // but this is expected as if it would have a similarity from tokens that have
@@ -274,8 +277,7 @@ export function mkInsertableTokenImporter(
       if (targetToken.type !== oldToken.type) {
         return false;
       }
-
-      const isSameName = targetToken.name === oldToken.name;
+      const isSameName = targetToken.name === namespacedTokenName;
 
       if (
         tokenResolution === "reuse-by-name" ||
@@ -306,9 +308,7 @@ export function mkInsertableTokenImporter(
 
     const tplMgr = new TplMgr({ site: targetSite });
     const newToken = tplMgr.addToken({
-      name: tplMgr.getUniqueTokenName(
-        groupName ? `${groupName}/${oldToken.name}` : oldToken.name
-      ),
+      name: tplMgr.getUniqueTokenName(namespacedTokenName),
       tokenType: oldToken.type as TokenType,
       value: maybeDerefToken(targetTokens, oldTokens, oldToken),
     });
