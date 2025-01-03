117bbfdc6d8f2f87559e6d180e5a1266b2fc13e2
Felipe Mota <fmota@plasmic.app>
2025-01-02 22:05:32 -0300

    refactor(branching): Clean up merge virtual slots test
    GitOrigin-RevId: 05093ea1e476a82c2d3784870a809861fdffa958

--- platform/wab/src/wab/shared/site-diffs/_tests_/merge-virtual-slots.spec.ts
+++ platform/wab/src/wab/shared/site-diffs/_tests_/merge-virtual-slots.spec.ts
@@ -1,12 +1,13 @@
 import { ProjectFullDataResponse } from "@/wab/shared/ApiSchema";
 import { flattenComponent } from "@/wab/shared/cached-selectors";
-import { assert, ensure, hackyCast } from "@/wab/shared/common";
+import { assert, ensure, hackyCast, intersectSets } from "@/wab/shared/common";
 import { getParamByVarName } from "@/wab/shared/core/components";
 import { flattenTpls, isTplNamable } from "@/wab/shared/core/tpls";
 import {
   isKnownTplComponent,
   isKnownVirtualRenderExpr,
   Site,
+  TplComponent,
   TplNode,
 } from "@/wab/shared/model/classes";
 import defaultSlotChangeBundle from "@/wab/shared/site-diffs/_tests_/bundles/default-slot-change.json";
@@ -19,6 +20,69 @@ import { getSlotArg } from "@/wab/shared/SlotUtils";
 beforeAll(async () => {
   await fetchLastBundleVersion();
 });
+
+function getComponentInstance(site: Site) {
+  const homepage = ensure(
+    site.components.find((c) => c.name === "/"),
+    "Couldn't find Homepage"
+  );
+  const instance = ensure(
+    flattenComponent(homepage).find(
+      (tpl) => isTplNamable(tpl) && tpl.name === "wrappedInstance"
+    ),
+    "Couldn't find wrappedInstance"
+  );
+
+  assert(isKnownTplComponent(instance), "Found instance is not of a component");
+
+  return instance;
+}
+
+function getSlotParamContent(
+  ancestorInstance: TplComponent,
+  mergedInstance: TplComponent,
+  slot: "target" | "content"
+) {
+  const ancestorParam = ensure(
+    getParamByVarName(ancestorInstance.component, slot),
+    `Couldn't find ancestor ${slot} param`
+  );
+
+  const mergedParam = ensure(
+    getParamByVarName(mergedInstance.component, slot),
+    `Couldn't find merged ${slot} param`
+  );
+
+  expect(ancestorParam.uuid).toEqual(mergedParam.uuid);
+
+  const ancestorSlot = ensure(
+    getSlotArg(ancestorInstance, ancestorParam),
+    `Couldn't find ancestor ${slot} slot`
+  ).expr;
+  const mergedSlot = ensure(
+    getSlotArg(mergedInstance, mergedParam),
+    `Couldn't find merged ${slot} slot`
+  ).expr;
+
+  assert(
+    isKnownVirtualRenderExpr(ancestorSlot),
+    `Ancestor ${slot} content should be virtual render expr`
+  );
+
+  assert(
+    isKnownVirtualRenderExpr(mergedSlot),
+    `Merged ${slot} content should be virtual render expr`
+  );
+
+  return {
+    ancestorSlot,
+    mergedSlot,
+  };
+}
+
+function uuidsOfFlattenedTpls(tpls: TplNode[]) {
+  return tpls.flatMap((tpl) => flattenTpls(tpl).map(({ uuid }) => uuid));
+}
 
 describe("Merging virtual slots", () => {
   it("shouldn't overwrite args for unchanged slots", () => {
@@ -50,76 +114,14 @@ describe("Merging virtual slots", () => {
       status: "merged",
     });
 
-    function getComponentInstance(site: Site) {
-      const homepage = ensure(
-        site.components.find((c) => c.name === "/"),
-        "Couldn't find Homepage"
-      );
-      const instance = ensure(
-        flattenComponent(homepage).find(
-          (tpl) => isTplNamable(tpl) && tpl.name === "wrappedInstance"
-        ),
-        "Couldn't find wrappedInstance"
-      );
-
-      assert(
-        isKnownTplComponent(instance),
-        "Found instance is not of a component"
-      );
-
-      return instance;
-    }
-
     const ancestor = result.ancestorSite;
     const merged = result.mergedSite;
 
     const ancestorInstance = getComponentInstance(ancestor);
     const mergedInstance = getComponentInstance(merged);
 
-    function getSlotParamContent(slot: "target" | "content") {
-      const ancestorParam = ensure(
-        getParamByVarName(ancestorInstance.component, slot),
-        `Couldn't find ancestor ${slot} param`
-      );
-
-      const mergedParam = ensure(
-        getParamByVarName(mergedInstance.component, slot),
-        `Couldn't find merged ${slot} param`
-      );
-
-      expect(ancestorParam.uuid).toEqual(mergedParam.uuid);
-
-      const ancestorSlot = ensure(
-        getSlotArg(ancestorInstance, ancestorParam),
-        `Couldn't find ancestor ${slot} slot`
-      ).expr;
-      const mergedSlot = ensure(
-        getSlotArg(mergedInstance, mergedParam),
-        `Couldn't find merged ${slot} slot`
-      ).expr;
-
-      assert(
-        isKnownVirtualRenderExpr(ancestorSlot),
-        `Ancestor ${slot} content should be virtual render expr`
-      );
-
-      assert(
-        isKnownVirtualRenderExpr(mergedSlot),
-        `Merged ${slot} content should be virtual render expr`
-      );
-
-      return {
-        ancestorSlot,
-        mergedSlot,
-      };
-    }
-
-    function uuidsOfFlattenedTpls(tpls: TplNode[]) {
-      return tpls.flatMap((tpl) => flattenTpls(tpl).map(({ uuid }) => uuid));
-    }
-
     const { ancestorSlot: ancestorSlotTarget, mergedSlot: mergedSlotTarget } =
-      getSlotParamContent("target");
+      getSlotParamContent(ancestorInstance, mergedInstance, "target");
 
     // Since this slot wasn't changed, we should expect all the uuids to be the same
     expect(uuidsOfFlattenedTpls(mergedSlotTarget.tpl)).toEqual(
@@ -127,7 +129,7 @@ describe("Merging virtual slots", () => {
     );
 
     const { ancestorSlot: ancestorSlotContent, mergedSlot: mergedSlotContent } =
-      getSlotParamContent("content");
+      getSlotParamContent(ancestorInstance, mergedInstance, "content");
 
     const uuidsAncestorSlotContent = uuidsOfFlattenedTpls(
       ancestorSlotContent.tpl
@@ -140,10 +142,11 @@ describe("Merging virtual slots", () => {
 
     // All the uuids should be different because we changed the default value of the text node in the `content` slot
     // which triggered the default content to be resynced
-    for (let i = 0; i < uuidsAncestorSlotContent.length; i++) {
-      expect(uuidsAncestorSlotContent[i]).not.toEqual(
-        uuidsMergedSlotContent[i]
-      );
-    }
+    expect(
+      intersectSets(
+        new Set(uuidsAncestorSlotContent),
+        new Set(uuidsMergedSlotContent)
+      ).size
+    ).toEqual(0);
   });
 });
