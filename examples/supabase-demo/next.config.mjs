85f60327b38f28656f3b493fbfa0056546b58bc9
Alex Noel <auzaka@gmail.com>
2025-01-21 17:43:56 +0100

    chore(examples): supabase-demo, upgrade packages

---
+++ examples/supabase-demo/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  trailingSlash: false,
+  reactStrictMode: true,
+};
+
+export default nextConfig;
