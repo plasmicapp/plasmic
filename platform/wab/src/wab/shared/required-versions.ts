26ad84c339fbd51f245c956fb12f978089869cda
Felipe Mota <fmota@plasmic.app>
2025-02-20 15:32:47 -0300

    chore: Bump required version of @plasmicapp/host (#513)
    GitOrigin-RevId: cc2ad5da7d833335418f24bfe4be1d20b8054132

--- platform/wab/src/wab/shared/required-versions.ts
+++ platform/wab/src/wab/shared/required-versions.ts
@@ -1,7 +1,7 @@
 export const requiredPackageVersions = {
   "@plasmicapp/loader": "0.0.1",
   "@plasmicapp/cli": "0.1.324",
-  "@plasmicapp/host": "1.0.188",
+  "@plasmicapp/host": "1.0.211",
   "@plasmicapp/react-web": "0.2.321",
   "@plasmicapp/react-web-runtime": "0.0.1",
 };
