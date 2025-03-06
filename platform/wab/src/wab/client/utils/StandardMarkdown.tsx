534d94776757f22f250c91849e5f0bec556bc124
Muhammad Asim <asimkhan7330.ak@gmail.com>
2025-03-06 22:43:37 +0500

    [PLA-11793]: Preview mentions in the comment view markdown (#549)
    * feat(user-mentions): added user mentions comment input in add/edit comment
    
    chore(comments): used named import for the UserMentionPopoverContent
    
    refactor(comment-post): reuse CommentPostForm in the CommentPost for editing
    
    chore(comments-user-mentions): removed unused dependencies and anonymous function
    
    chore(user-mentions): sync UserMentionDisplay component from User Mentions project
    
    chore(user-mentions): sync UserMentionDisplay component changes
    
    feat(user-mentions): preview email user mention with user full name
    
    * chore(user-mentions-preview): update UserMentionsDisplay span element in the studio and sync changes
    
    * refactor(user-mentions-preview): add default link display for non project users email in mentions
    
    * feat(user-mentions-preview): yarn.lock changes after rebase
    
    * fix(user-mentions-preview): fixed remark-gfm version conflict with react-markdown breaking existing use cases
    
    GitOrigin-RevId: 227b4fed659342a036c0d3979850086e557db80d

--- platform/wab/src/wab/client/utils/StandardMarkdown.tsx
+++ platform/wab/src/wab/client/utils/StandardMarkdown.tsx
@@ -1,6 +1,55 @@
+import { UserMentionDisplay } from "@/wab/client/components/user-mentions/UserMentionDisplay";
 import React, { ComponentProps } from "react";
 import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export function StandardMarkdown(props: ComponentProps<typeof ReactMarkdown>) {
-  return <ReactMarkdown linkTarget={"_blank"} {...props} />;
+  const processedChildren =
+    typeof props.children === "string"
+      ? processUserMentions(props.children)
+      : props.children;
+
+  function processUserMentions(content: string) {
+    // Remove the leading @ from the mention @email
+    const mentionRegex = /@(\S+@\S+\.\S+)/gm;
+    return content.replace(mentionRegex, (_match, email) => {
+      return email;
+    });
+  }
+
+  return (
+    <ReactMarkdown
+      linkTarget={"_blank"}
+      remarkPlugins={[remarkGfm]}
+      components={{
+        a: ({ href, children, ...restProps }) => {
+          const defaultLinkNode = (
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              {...restProps}
+            >
+              {children}
+            </a>
+          );
+
+          const isEmailLink = href?.startsWith("mailto:");
+          if (isEmailLink) {
+            const email = href!.replace("mailto:", "");
+            return (
+              <UserMentionDisplay
+                email={email}
+                defaultEmailDisplay={defaultLinkNode}
+              />
+            );
+          }
+
+          return defaultLinkNode;
+        },
+      }}
+      {...props}
+      children={processedChildren}
+    />
+  );
 }
