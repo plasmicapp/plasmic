66a795caa200ce857f875ec898d0df9262ef3240
Felipe Mota <fmota@plasmic.app>
2025-03-07 15:28:10 -0300

    [PLA-11803] feat(rsc): Add hostless function package (#569)
    * feat(rsc): Add hostless function package
    
    * Add plasmicpkgs/fetch in workspaces
    
    * fix: Additional build for api.md
    
    GitOrigin-RevId: 8b1546e1945e85a754582758b4fa1f89e9ab46d0

---
+++ plasmicpkgs/fetch/src/index.ts
@@ -0,0 +1,99 @@
+import registerFunction, {
+  CustomFunctionMeta,
+} from "@plasmicapp/host/registerFunction";
+
+type Registerable = {
+  registerFunction: typeof registerFunction;
+};
+
+// Some functions were extracted from platform/wab/src/wab/server/data-sources/http-fetcher.ts
+
+type HTTPMethod = "GET" | "POST" | "PUT" | "DELETE";
+
+function base64StringToBuffer(bstr: string) {
+  try {
+    bstr = atob(bstr);
+  } catch (e) {
+    throw new Error("Invalid base64 for binary type");
+  }
+  const uint8Array = new Uint8Array(bstr.length);
+  for (let i = 0; i < bstr.length; i++) {
+    uint8Array[i] = bstr.charCodeAt(i);
+  }
+  return uint8Array.buffer;
+}
+
+function bodyToFetchBody(body?: string | object) {
+  if (body == null) {
+    return undefined;
+  } else if (typeof body === "object") {
+    return JSON.stringify(body);
+  } else if (body.startsWith("@binary")) {
+    return base64StringToBuffer(body.slice("@binary".length));
+  } else {
+    return body;
+  }
+}
+
+function maybeParseJSON(json: string) {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return json;
+  }
+}
+
+async function wrappedFetch(
+  url: string,
+  method: HTTPMethod,
+  headers: Record<string, string>,
+  body?: string | object
+) {
+  const response = await fetch(url, {
+    method,
+    headers,
+    body: bodyToFetchBody(body),
+  });
+
+  const statusCode = response.status;
+  const responseText = await response.text();
+
+  return {
+    statusCode,
+    headers: Object.fromEntries(response.headers.entries()),
+    response: maybeParseJSON(responseText),
+  };
+}
+
+export { wrappedFetch as fetch };
+
+const registerFetchParams: CustomFunctionMeta<typeof wrappedFetch> = {
+  name: "fetch",
+  importPath: "@plasmicapp/fetch",
+  params: [
+    {
+      name: "url",
+      type: "string",
+    },
+    {
+      name: "method",
+      type: "string",
+    },
+    {
+      name: "headers",
+      type: "object",
+    },
+    {
+      name: "body",
+      type: "object",
+    },
+  ],
+};
+
+export function registerFetch(loader?: Registerable) {
+  if (loader) {
+    loader.registerFunction(wrappedFetch, registerFetchParams);
+  } else {
+    registerFunction(wrappedFetch, registerFetchParams);
+  }
+}
