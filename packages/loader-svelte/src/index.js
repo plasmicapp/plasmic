c67a36ed2c375819aead3a0ad8901325f47910a6
Jason Long <j@jaslong.com>
2025-05-02 19:30:14 -0700

    deprecate loader-svelte
    Change-Id: I82a690bf3367f16f074c215deafb535828930200
    GitOrigin-RevId: 81edda9006e6e4b26f218bbf24eb8f37462b151d

--- packages/loader-svelte/src/index.js
+++
@@ -1,5 +0,0 @@
-import { initPlasmicLoader } from "@plasmicapp/loader-react";
-import PlasmicComponent from "./components/PlasmicComponent.svelte";
-import PlasmicRootProvider from "./components/PlasmicRootProvider.svelte";
-
-export { initPlasmicLoader, PlasmicComponent, PlasmicRootProvider };
