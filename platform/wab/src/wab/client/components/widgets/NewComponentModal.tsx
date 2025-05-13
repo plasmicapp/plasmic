9a438ba20a36e595f4e11ff624799405a1d0115c
Sarah Ahmed <sarahsga@users.noreply.github.com>
2025-05-13 14:45:29 +0500

    fix(wab): not show plume components in New Component modal (#1021)
    Change-Id: I17ef6421f2e7dca206a83ea8f025fd995e8595a4
    GitOrigin-RevId: 71c3a833b6b4b646dd46cd82ffef13314c17ba60

--- platform/wab/src/wab/client/components/widgets/NewComponentModal.tsx
+++ platform/wab/src/wab/client/components/widgets/NewComponentModal.tsx
@@ -1,8 +1,3 @@
-import {
-  getPlumeComponentTemplates,
-  getPlumeImage,
-} from "@/wab/client/components/plume/plume-display-utils";
-import { Icon } from "@/wab/client/components/widgets/Icon";
 import NewComponentItem from "@/wab/client/components/widgets/NewComponentItem";
 import NewComponentSection from "@/wab/client/components/widgets/NewComponentSection";
 import { TextboxRef } from "@/wab/client/components/widgets/Textbox";
@@ -11,23 +6,18 @@ import {
   getInsertableTemplateComponentItem,
   getScreenVariantToInsertableTemplate,
 } from "@/wab/client/insertable-templates";
-import PlumeMarkIcon from "@/wab/client/plasmic/plasmic_kit_design_system/icons/PlasmicIcon__PlumeMark";
 import {
   DefaultNewComponentModalProps,
   PlasmicNewComponentModal,
 } from "@/wab/client/plasmic/plasmic_kit_new_component/PlasmicNewComponentModal";
 import { StudioCtx } from "@/wab/client/studio-ctx/StudioCtx";
-import { assert, ensure } from "@/wab/shared/common";
+import { assert } from "@/wab/shared/common";
 import { flattenInsertableTemplatesByType } from "@/wab/shared/devflags";
 import { InsertableTemplateComponentExtraInfo } from "@/wab/shared/insertable-templates/types";
-import { PLEXUS_INSERTABLE_ID } from "@/wab/shared/insertables";
-import { getPlumeEditorPluginByType } from "@/wab/shared/plume/plume-registry";
-import { Tooltip } from "antd";
 import * as React from "react";
 
 export type NewComponentInfo = {
   name: string;
-  plumeTemplateId?: string;
   insertableTemplateInfo?: InsertableTemplateComponentExtraInfo;
 };
 
@@ -42,26 +32,15 @@ function NewComponentModal(props: NewComponentModalProps) {
   const { onSubmit, onCancel, studioCtx, ...rest } = props;
 
   const [expanded, setExpanded] = React.useState(false);
-  const [templateId, setTemplateId] = React.useState<string | undefined>(
+  const [templateName, setTemplateName] = React.useState<string | undefined>(
     undefined
   );
   const [name, setName] = React.useState("");
   const nameRef = React.useRef<TextboxRef>(null);
 
-  const plumeSite = studioCtx.projectDependencyManager.plumeSite;
-  const plumeTemplates = getPlumeComponentTemplates(studioCtx);
-
-  // This is a temporary flag to hide the Plexus Design System for installation until it is ready for public use.
-  // Once Plexus is released for all users, this flag can be removed.
-  const hasPlexus = studioCtx.appCtx.appConfig.plexus;
-
-  const otherTemplates = flattenInsertableTemplatesByType(
+  const templates = flattenInsertableTemplatesByType(
     studioCtx.appCtx.appConfig.insertableTemplates,
     "insertable-templates-component"
-  ).filter(
-    // Show Plexus components only to users who are allowed access to them
-    (i) =>
-      i.templateName.startsWith(`${PLEXUS_INSERTABLE_ID}/`) ? hasPlexus : true
   );
 
   return (
@@ -72,8 +51,7 @@ function NewComponentModal(props: NewComponentModalProps) {
           onSubmit: async (e: Event) => {
             e.preventDefault();
             if (name) {
-              if (templateId?.startsWith("template:")) {
-                const templateName = templateId.split(":")[1];
+              if (templateName) {
                 const templateItem = getInsertableTemplateComponentItem(
                   studioCtx,
                   templateName
@@ -87,8 +65,6 @@ function NewComponentModal(props: NewComponentModalProps) {
                   screenVariant
                 );
                 onSubmit({ name, insertableTemplateInfo: templateInfo });
-              } else if (templateId) {
-                onSubmit({ name, plumeTemplateId: templateId });
               } else {
                 onSubmit({ name });
               }
@@ -129,83 +105,39 @@ function NewComponentModal(props: NewComponentModalProps) {
     >
       <NewComponentSection>
         <NewComponentItem
-          isSelected={templateId == null}
+          isSelected={templateName == null}
           title="Blank component"
           onClick={() => {
-            const previousName = plumeTemplates.find(
-              (c) => c.uuid === templateId
-            )?.name;
-            setTemplateId(undefined);
-            if (previousName === name) {
+            const previousTemplate = templates.find(
+              (c) => c.templateName === templateName
+            );
+            if (previousTemplate?.componentName === name) {
               setName("");
             }
+            setTemplateName(undefined);
             if (nameRef.current) {
               nameRef.current.focus();
             }
           }}
         />
       </NewComponentSection>
-      {plumeSite && (
-        <NewComponentSection
-          title={
-            <Tooltip
-              title={
-                "These components come with interactivity and accessibility built-in."
-              }
-            >
-              <span>
-                <Icon icon={PlumeMarkIcon} /> Interactive components
-              </span>
-            </Tooltip>
-          }
-        >
-          {plumeTemplates.map((comp, i) => {
-            const plumeType = ensure(
-              comp.plumeInfo,
-              "Plume component should have plumeInfo"
-            ).type;
-            // Do not show Plume components of kinds that are already
-            // available on site default components.
-            if (studioCtx.site.defaultComponents[plumeType]) {
-              return null;
-            }
-            const plugin = getPlumeEditorPluginByType(plumeType);
-            return (
-              <NewComponentItem
-                key={`${i}`}
-                isSelected={comp.uuid === templateId}
-                title={comp.name}
-                imgUrl={getPlumeImage(plumeType)}
-                tooltip={plugin?.componentMeta.description}
-                onClick={() => {
-                  const previousName = plumeTemplates.find(
-                    (c) => c.uuid === templateId
-                  )?.name;
-                  setTemplateId(comp.uuid);
-                  if (!name || previousName === name) {
-                    setName(comp.name);
-                  }
-                  if (nameRef.current) {
-                    nameRef.current.focus();
-                  }
-                }}
-              />
-            );
-          })}
-        </NewComponentSection>
-      )}
-      {otherTemplates.length > 0 && (
+      {templates.length > 0 && (
         <NewComponentSection title={"Common components"}>
-          {otherTemplates.map((template) => {
-            const thisTemplateId = `template:${template.templateName}`;
+          {templates.map((template) => {
+            const thisTemplateName = template.templateName;
             return (
               <NewComponentItem
-                isSelected={templateId === thisTemplateId}
+                isSelected={templateName === thisTemplateName}
                 title={template.displayName ?? template.componentName}
                 imgUrl={template.imageUrl}
                 onClick={() => {
-                  setName(template.componentName);
-                  setTemplateId(thisTemplateId);
+                  const previousTemplate = templates.find(
+                    (c) => c.templateName === templateName
+                  );
+                  if (!name || previousTemplate?.componentName === name) {
+                    setName(template.componentName);
+                  }
+                  setTemplateName(thisTemplateName);
                   if (nameRef.current) {
                     nameRef.current.focus();
                   }
