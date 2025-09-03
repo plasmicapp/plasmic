f187d908af3930a3ea731df460fc0ca3ea1b26d6
Ícaro Guerra <icaro@plasmic.app>
2025-09-03 13:41:52 -0300

    fix(observability): fix public observability to have Pino logger
    GitOrigin-RevId: 0f0469416f6811a1426422f544c8bd4b1f495a42

--- platform/wab/src/wab/server/observability/index.ts
+++ platform/wab/src/wab/server/observability/index.ts
@@ -1,8 +1,16 @@
 import { methodForwarder } from "@/wab/commons/methodForwarder";
+import { PinoLogger } from "@/wab/server/observability/PinoLogger";
 import { Analytics } from "@/wab/shared/observability/Analytics";
+import { Logger } from "@/wab/shared/observability/Logger";
+
+const loggerInstance: Logger = new PinoLogger();
 
 export function initAnalyticsFactory(opts: {
   production: boolean;
 }): () => Analytics {
   return () => methodForwarder<Analytics>();
+}
+
+export function logger(): Logger {
+  return loggerInstance;
 }
