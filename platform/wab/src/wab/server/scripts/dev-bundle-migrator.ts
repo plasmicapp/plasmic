578a2a6036f9f45e1cdad8efe306d6840de1cb6e
Muhammad Asim <asimkhan7330.ak@gmail.com>
2026-01-02 14:45:46 +0500

    fix(dev-bundle-migration): fix path of transform bundle migration and source bundle in fixtures (#2061)
    GitOrigin-RevId: 9f7839790fb77e5f838f68e65d3eca5f74240fbc

--- platform/wab/src/wab/server/scripts/dev-bundle-migrator.ts
+++ platform/wab/src/wab/server/scripts/dev-bundle-migrator.ts
@@ -95,7 +95,7 @@ const paths = [
   "src/wab/server/__tests__/bundle-migrations/fixtures/duplicate-variants.migrated.json",
   "src/wab/server/__tests__/bundle-migrations/fixtures/merge-duplicate-variants.migrated.json",
   "src/wab/server/__tests__/bundle-migrations/fixtures/flex-gap-bundle.migrated.json",
-  "src/wab/server/__tests__/bundle-migrations/fixtures/transform-format-migration.json",
+  "src/wab/server/__tests__/bundle-migrations/fixtures/transform-format-migration.migrated.json",
 ];
 
 async function migrate() {
