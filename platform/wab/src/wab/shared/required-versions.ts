4465e985dc5eee1a7883f1fd94885be51a5f9917
Jason Long <j@jaslong.com>
2026-01-21 13:45:47 +0800

    Update @plasmicapp/cli required version (#2153)
    To speed up adoption of new CLI to avoid 413 errors
    
    GitOrigin-RevId: 73a262a5e32df3d5b0ef3aaf184f557d7c35d721

--- platform/wab/src/wab/shared/required-versions.ts
+++ platform/wab/src/wab/shared/required-versions.ts
@@ -1,6 +1,6 @@
 export const requiredPackageVersions = {
   "@plasmicapp/loader": "0.0.1",
-  "@plasmicapp/cli": "0.1.342",
+  "@plasmicapp/cli": "0.1.352",
   "@plasmicapp/host": "1.0.211",
   "@plasmicapp/react-web": "0.2.404",
   "@plasmicapp/react-web-runtime": "0.0.1",
