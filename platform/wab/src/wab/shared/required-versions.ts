24cb1130c59d886ef5b50fa9de68445dd738a183
Sarah Ahmed <sarahsga@users.noreply.github.com>
2026-02-16 12:47:02 +0500

    chore: bump react-web required version (#2253)
    GitOrigin-RevId: 1a2027ad90a9c04cb8746d6eaedaea05e46977e4

--- platform/wab/src/wab/shared/required-versions.ts
+++ platform/wab/src/wab/shared/required-versions.ts
@@ -2,6 +2,6 @@ export const requiredPackageVersions = {
   "@plasmicapp/loader": "0.0.1",
   "@plasmicapp/cli": "0.1.352",
   "@plasmicapp/host": "1.0.211",
-  "@plasmicapp/react-web": "0.2.418",
+  "@plasmicapp/react-web": "0.2.422",
   "@plasmicapp/react-web-runtime": "0.0.1",
 };
