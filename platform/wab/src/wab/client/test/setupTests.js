28950496b671601fef0db56543537142d6ec9c09
Sam Pullman <sampullman@berkeley.edu>
2026-08-05 15:56:49 +0200

    Use indexdb for copilot chat storage (#3090)
    * feat: use indexdb for copilot chat storage
    
    * fix: pr review feedback and cleanup
    
    * feat: cross tab sync for copilot chat local persistence
    
    * fix: address more review comments and refactor validation
    
    * refactor: rename restore-chat-message to chat-serialization and fix conflicts
    
    * fix: block chat before restore and use fake-indexeddb in test
    
    * test: add better concurrent save copilot chat test
    
    GitOrigin-RevId: 17aecdd0ddffb0de41acf3ecceef234b26c53bf8

--- platform/wab/src/wab/client/test/setupTests.js
+++ platform/wab/src/wab/client/test/setupTests.js
@@ -2,7 +2,14 @@ import { DEVFLAGS } from "@/wab/shared/devflags";
 import "jest-extended";
 import { TransformStream } from "stream/web";
 import { TextDecoder, TextEncoder } from "util";
+import { deserialize, serialize } from "v8";
 import "whatwg-fetch";
+import { BroadcastChannel as NodeBroadcastChannel } from "worker_threads";
+
+// jest.config.ts defines `structuredClone` as an unusable placeholder object.
+// TODO - remove that clone override and delete this.
+global.structuredClone = (value) => deserialize(serialize(value));
+require("fake-indexeddb/auto");
 
 import "@/wab/client/test/initTests";
 
@@ -21,6 +28,15 @@ DEVFLAGS.hostUrl = "http://localhost";
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 global.TransformStream = TransformStream;
+
+// Node's BroadcastChannel is Web-compatible. `unref` so open channels don't keep
+// the process alive after tests.
+global.BroadcastChannel = class BroadcastChannel extends NodeBroadcastChannel {
+  constructor(name) {
+    super(name);
+    this.unref();
+  }
+};
 
 let objectURLCounter = 0;
 global.URL.createObjectURL = (object) => {
