231e7e2f579c4bb671bd34987bb9358eb3e1d0db
Daniel <daniel@schub.cloud>
2026-08-24 16:11:51 -0300

    [DEVOP-530]: Change deploy-spa.sh to modify spa deploy behavior (#3194)
    * Change deploy-spa.sh to modify spa deploy behavior
    
    * Add change just to trigger the build
    
    * Removing comment.
    
    * Address code review issue
    
    * Address code review issue
    
    * Address code review issue
    
    * Added a comment explaining why exclude these files
    
    GitOrigin-RevId: ddff5b4773f5faabf4f5f0981723944e68acf24f

--- scripts/deploy-spa.sh
+++ scripts/deploy-spa.sh
@@ -9,11 +9,33 @@ echo "  Project: $GCP_PROJECT"
 
 cd "$SOURCE_PATH"
 
+IMMUTABLE='public, max-age=31536000, immutable'
+SHORT='public, max-age=3600'
+
+# Exclude stable URLs from the immutable sync so they are uploaded only once,
+# below, with their final cache policy. Do not add a leading ^: gcloud treats it
+# as custom-delimiter syntax, while its regex matching is already anchored.
+EXCLUDE='(\./)?(index\.html|[^/]*\.worker\.js.*|favicon\.ico|robots\.txt|static/img/.*|static/font-awesome/.*|static/css/normalize\.css|static/js/getlibs\.js|static/js/preamble\.js|static/js/loader-hydrate\.js.*|static/js/studio\.js|static/host\.html|static/popup\.html)$'
+
 echo "Pushing version to GCS..."
 gcloud storage cp -r --quiet ./ "gs://$GCS_BUCKET/versions/$TAG/"
 
 echo "Deploying to root..."
-gcloud storage cp -r --quiet ./ "gs://$GCS_BUCKET/"
+gcloud storage rsync --recursive --quiet --cache-control "$IMMUTABLE" --exclude="$EXCLUDE" ./ "gs://$GCS_BUCKET/"
+
+# Override the cache policy for assets whose names are stable across deployments.
+gcloud storage cp -r --quiet ./static/img ./static/font-awesome \
+  "gs://$GCS_BUCKET/static/" --cache-control "$SHORT"
+gcloud storage cp --quiet ./static/css/normalize.css \
+  "gs://$GCS_BUCKET/static/css/" --cache-control "$SHORT"
+gcloud storage cp --quiet ./static/js/getlibs.js ./static/js/preamble.js \
+  ./static/js/loader-hydrate.js* "gs://$GCS_BUCKET/static/js/" \
+  --cache-control "$SHORT"
+gcloud storage cp --quiet ./static/host.html ./static/popup.html \
+  "gs://$GCS_BUCKET/static/" --cache-control "$SHORT"
+gcloud storage cp --quiet ./*.worker.js* ./favicon.ico ./robots.txt \
+  "gs://$GCS_BUCKET/" --cache-control "$SHORT"
+
 gcloud storage cp -r --quiet ./index.html "gs://$GCS_BUCKET/index.html" --cache-control 'max-age=0, s-maxage=31536000'
 gcloud storage cp -r --quiet ./static/js/studio.js "gs://$GCS_BUCKET/static/js/studio.js" --cache-control 'max-age=0, s-maxage=31536000'
 
